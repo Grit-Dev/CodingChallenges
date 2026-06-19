baa07790db72535bffbf92459d66997f54bd089d
Paul McGinley <paulmcginley16@outlook.com>
2026-06-19 09:36:49 +0100

    Most Freqwuent Number in Array

--- CodingChallenges/Program.cs
+++ CodingChallenges/Program.cs
@@ -56,6 +56,40 @@
         return newArray;
     }
 
+    public static int MostFrequentNumberInArrayRevision(int[] pInputValue)
+    {
+        if (pInputValue == null || pInputValue.Length == 0)
+        {
+            return 0;
+        };
+
+        int mostFrequentNumber = 0;
+
+        for (int counter = 0; counter <= pInputValue.Length - 1; counter++)
+        {
+
+            int count = 0;
+            int value = pInputValue[counter];
+
+            for (int counterTwo = 0; counterTwo <= pInputValue.Length - 1; counterTwo++)
+            {
+                if (value == pInputValue[counterTwo])
+                {
+                    count++;
+                }
+            }
+
+            if (count > mostFrequentNumber)
+            {
+                mostFrequentNumber = value;
+            }
+
+        }
+
+        return mostFrequentNumber;
+    }
+
+
     public static void Main(string[] args)
     {
         // Count Non Zeros: 
@@ -70,11 +104,18 @@
         //Console.WriteLine(string.Join(",", RotateArrayRightByOne([1])));          // 1
         //Console.WriteLine(string.Join(",", RotateArrayRightByOne([])));            // (empty)
 
-        //Rotate Array left by one:
-        Console.WriteLine(string.Join(",", RotateArrayLeftWarmUp([1, 2, 3, 4]))); // 2,3,4,1
-        Console.WriteLine(string.Join(",", RotateArrayLeftWarmUp([10, 20, 30]))); // 20,30,10
-        Console.WriteLine(string.Join(",", RotateArrayLeftWarmUp([1])));          // 1
-        Console.WriteLine(string.Join(",", RotateArrayLeftWarmUp([])));            // (empty)
+        // Rotate Array left by one:
+        //Console.WriteLine(string.Join(",", RotateArrayLeftWarmUp([1, 2, 3, 4]))); // 2,3,4,1
+        //Console.WriteLine(string.Join(",", RotateArrayLeftWarmUp([10, 20, 30]))); // 20,30,10
+        //Console.WriteLine(string.Join(",", RotateArrayLeftWarmUp([1])));          // 1
+        //Console.WriteLine(string.Join(",", RotateArrayLeftWarmUp([])));            // (empty)
+
+        // Most Frequent Number:
+        Console.WriteLine(MostFrequentNumberInArrayRevision([1, 2, 2, 3, 2])); // 2
+        Console.WriteLine(MostFrequentNumberInArrayRevision([5, 5, 1, 1]));    // 5
+        Console.WriteLine(MostFrequentNumberInArrayRevision([9]));             // 9
+        Console.WriteLine(MostFrequentNumberInArrayRevision([]));               // 0
+
 
     }
 }
