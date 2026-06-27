2018a4a0aa37c1e5264e5cc920716432fef2a67a
Paul McGinley <paulmcginley16@outlook.com>
2026-06-27 07:08:20 +0100

    CountDigitsInString and RemoveVowelsPreservingCasing

--- CodingChallenges/Program.cs
+++ CodingChallenges/Program.cs
@@ -21,13 +21,43 @@ public class Program
 
         return count;
     }
+
+    public static string RemoveVowelsPreserveCasing(string pInputValue)
+    {
+        if (string.IsNullOrEmpty(pInputValue))
+        {
+            return "";
+        }
+
+        char[] vowels = ['a', 'e', 'i', 'o', 'u'];
+        StringBuilder stringBuilder = new();
+
+        foreach (char character in pInputValue)
+        {
+            if (!vowels.Contains(char.ToLower(character)))
+            {
+                stringBuilder.Append(character);
+            }
+        }
+
+        return stringBuilder.ToString();
+    }
+
     public static void Main(string[] args)
     {
+        // Remove Vowels Preserving Casing
+        Console.WriteLine(RemoveVowelsPreserveCasing("hello"));
+        Console.WriteLine(RemoveVowelsPreserveCasing("Cyberpunk 2077"));
+        Console.WriteLine(RemoveVowelsPreserveCasing("APPLE"));
+        Console.WriteLine(RemoveVowelsPreserveCasing("Why"));
+        Console.WriteLine(RemoveVowelsPreserveCasing(null!));
 
-        Console.WriteLine(CountDigitsInString("abc123"));
-        Console.WriteLine(CountDigitsInString("Cyberpunk 2077"));
-        Console.WriteLine(CountDigitsInString("hello"));
-        Console.WriteLine(CountDigitsInString(""));
-        Console.WriteLine(CountDigitsInString(null!));
+
+        // Count Digits in String
+        //Console.WriteLine(CountDigitsInString("abc123"));
+        //Console.WriteLine(CountDigitsInString("Cyberpunk 2077"));
+        //Console.WriteLine(CountDigitsInString("hello"));
+        //Console.WriteLine(CountDigitsInString(""));
+        //Console.WriteLine(CountDigitsInString(null!));
     }
 }
