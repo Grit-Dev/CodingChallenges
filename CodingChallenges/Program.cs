ec85f7540c67e0c6aed8aab30a1238777b7a5e74
Grit-Dev <paulmcginley16@outlook.com>
2026-08-04 16:27:53 +0100

    Challenge 2 - MaskCodeExceptionLastThree: completed with console.writelines

--- CodingChallenges/Program.cs
+++ CodingChallenges/Program.cs
@@ -26,14 +26,45 @@ public class Program
 
         return counter;
     }
+
+    public static string MaskCodeExceptLastThree(string code)
+    {
+        if(string.IsNullOrWhiteSpace(code))
+        {
+            return "";
+        }
+
+        if(code.Length <= 3)
+        {
+            return code;
+        }
+
+        int lengthLeft = code.Length - 3;
+
+        string asterisks = new('*', lengthLeft);
+
+        string lastThreeDigits = code.Substring(lengthLeft);
+
+        return asterisks + lastThreeDigits;
+    }
     public static void Main(string[] args)
     {
+
+        // Mask Code Except Last Three
+        Console.WriteLine(MaskCodeExceptLastThree("ABCDEFG") == "****EFG");
+        Console.WriteLine(MaskCodeExceptLastThree("123456") == "***456");
+        Console.WriteLine(MaskCodeExceptLastThree("ABC") == "ABC");
+        Console.WriteLine(MaskCodeExceptLastThree("AB") == "AB");
+        Console.WriteLine(MaskCodeExceptLastThree(null!) == "");
+        Console.WriteLine(MaskCodeExceptLastThree("") == "");
+
+
         // Count Valid Prices
-        Console.WriteLine(CountValidPrices("10, 20, 0, -5, hello") == 2);
-        Console.WriteLine(CountValidPrices("0, -1, abc") == 0);
-        Console.WriteLine(CountValidPrices(" 5, 15 , test, 25 ")== 3);
-        Console.WriteLine(CountValidPrices("")== 0);
-        Console.WriteLine(CountValidPrices(null!)== 0);
+        // Console.WriteLine(CountValidPrices("10, 20, 0, -5, hello") == 2);
+        // Console.WriteLine(CountValidPrices("0, -1, abc") == 0);
+        // Console.WriteLine(CountValidPrices(" 5, 15 , test, 25 ")== 3);
+        // Console.WriteLine(CountValidPrices("")== 0);
+        // Console.WriteLine(CountValidPrices(null!)== 0);
 
         //JsonDataSanitizerRunner.Run();
     }
