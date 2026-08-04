a74559c4b5aedfe4291613f5e58b7304ecefbce4
Grit-Dev <paulmcginley16@outlook.com>
2026-08-04 10:56:53 +0100

    test: more tests
    refactor: fixed string.empty with person class. There was a space

--- CodingChallenges/Challenges/Phase_03_Practical_Challenges/PersonalDataRedactorTests.cs
+++ CodingChallenges/Challenges/Phase_03_Practical_Challenges/PersonalDataRedactorTests.cs
@@ -33,21 +33,110 @@ namespace CodingChallenges.Challenges.Phase_03_Practical_Challenges
             Assert.DoesNotContain("derek.morgan@example.co.uk", actualResult);
         }
 
-        [Fact(Skip = "TODO: implement test")]
+        [Fact]
         public void RedactPersonalData_WithMcSurname_IgnoresMcForSurnameInitial()
         {
+            // Arrange
+            var json = @"{
+                    ""people"": [
+                        {
+                            ""name"": ""Derek McMorgan"",
+                            ""address"": """",
+                            ""mobile"": """",
+                            ""emailAddress"": """"
+                        }
+                    ]
+                }";
+
+            // Act 
+            var actualResult = PersonalDataRedactor.RedactPersonalData(json);
+
+            // Assert
+            Assert.Contains(@"""name"":""DM""", actualResult);
+            Assert.DoesNotContain("Derek Morgan", actualResult);
         }
 
-        [Fact(Skip = "TODO: implement test")]
+        [Fact]
         public void RedactPersonalData_WithSingleName_ReturnsSingleInitial()
         {
+            // Arrange
+            var json = @"{
+                    ""people"": [
+                        {
+                            ""name"": ""Derek"",
+                            ""address"": """",
+                            ""mobile"": """",
+                            ""emailAddress"": """"
+                        }
+                    ]
+                }";
+
+            // Act 
+            var actualResult = PersonalDataRedactor.RedactPersonalData(json);
+
+            // Assert
+            Assert.Contains(@"""name"":""D""", actualResult);
+            Assert.DoesNotContain("Derek", actualResult);
         }
 
-        [Fact(Skip = "TODO: implement test")]
+        [Fact]
         public void RedactPersonalData_WithMultiplePeople_RedactsAllPeople()
         {
-        }
+            // Arrange 
+            var json = @"{
+                ""people"": [
+                    {
+                        ""name"": ""Derek"",
+                        ""address"": ""4321, Edinburgh, EH13 767"",
+                        ""mobile"": ""07700900555"",
+                        ""emailAddress"": ""derek.morgan@example.co.uk""
+                    },
+                    {
+                        ""name"": ""Solid Snake"",
+                        ""address"": ""5678, London, EH14 777"",
+                        ""mobile"": ""07700900777"",
+                        ""emailAddress"": ""solid.snake@example.co.uk""
+                    },
+                    {
+                        ""name"": ""Geralt McRivia"",
+                        ""address"": ""1234, Belfast, EH15 888"",
+                        ""mobile"": ""07700900901"",
+                        ""emailAddress"": ""geralt.mcrivia@example.co.uk""
+                    }
+                ]
+            }";
 
+            // Act    
+            var actualResult = PersonalDataRedactor.RedactPersonalData(json);
+
+            // Assert
+
+            // Derek
+            Assert.Contains(@"""name"":""D""", actualResult);
+            Assert.Contains(@"""address"":""Edinburgh""", actualResult);
+            Assert.Contains(@"""mobile"":""*******0555""", actualResult);
+            Assert.Contains(@"""emailAddress"":""************@example.co.uk""", actualResult);
+
+            // Solid Snake
+            Assert.Contains(@"""name"":""SS""", actualResult);
+            Assert.Contains(@"""address"":""London""", actualResult);
+            Assert.Contains(@"""mobile"":""*******0777""", actualResult);
+            Assert.Contains(@"""emailAddress"":""***********@example.co.uk""", actualResult);
+
+            // Geralt McRivia
+            Assert.Contains(@"""name"":""GR""", actualResult);
+            Assert.Contains(@"""address"":""Belfast""", actualResult);
+            Assert.Contains(@"""mobile"":""*******0901""", actualResult);
+            Assert.Contains(@"""emailAddress"":""**************@example.co.uk""", actualResult);
+
+            Assert.DoesNotContain("07700900555", actualResult);
+            Assert.DoesNotContain("07700900777", actualResult);
+            Assert.DoesNotContain("07700900901", actualResult);
+
+            Assert.DoesNotContain("derek.morgan@example.co.uk", actualResult);
+            Assert.DoesNotContain("solid.snake@example.co.uk", actualResult);
+            Assert.DoesNotContain("geralt.mcrivia@example.co.uk", actualResult);
+        }
         [Fact(Skip = "TODO: implement test")]
         public void RedactPersonalData_WithEmptyJson_ReturnsEmptyString()
         {
