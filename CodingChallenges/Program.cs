94abff012f55591a0596ae57050b60f2ab4cdfe8
Paul McGinley <paulmcginley16@outlook.com>
2026-08-04 18:17:20 +0100

    Inital setup up of the challenge of Json

--- CodingChallenges/Program.cs
+++ CodingChallenges/Program.cs
@@ -1,8 +1,62 @@
-﻿public class Program
+﻿using CodingChallenges.Challenges.Phase_03_Practical_Challenges;
+using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities;
+using System.Text.Json;
+
+public class Program
 {
+    public class PersonData
+    {
+        public List<Person> People { get; set; } = [];
+    }
+    public class Person
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public string Address { get; set; } = string.Empty;
+
+        public string Mobile { get; set; } = string.Empty;
+
+        public string EmailAddress { get; set; } = string.Empty;
+    }
+    public static string RedactPersonalData(string json)
+    {
+        if (string.IsNullOrEmpty(json))
+        {
+            return "";
+        }
+
+        var options = new JsonSerializerOptions
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            PropertyNameCaseInsensitive = true,
+
+        };
+
+        var jsonDeserialized = JsonSerializer.Deserialize<PeopleData>(json, options);
+
+        if (jsonDeserialized == null || jsonDeserialized.People.Count == 0)
+        {
+            return "";
+        }
+
+        foreach (var person in jsonDeserialized.People)
+        {
+            person.Name = "";
+            person.Address = "";
+            person.Mobile = "";
+            person.EmailAddress = "";
+        }
+
+        var jsonSerialized = JsonSerializer.Serialize(jsonDeserialized);
+
+        return jsonSerialized;
+    }
     public static void Main(string[] args)
     {
+
+
         //JsonDataSanitizerRunner.Run();
     }
 
 }
+ 
