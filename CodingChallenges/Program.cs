4b42cca635ddae97696588c52c8de887e33a772f
Grit-Dev <paulmcginley16@outlook.com>
2026-08-05 15:46:24 +0100

    feat: added RedactedPersonData

--- CodingChallenges/Program.cs
+++ CodingChallenges/Program.cs
@@ -1,7 +1,48 @@
-﻿using CodingChallenges.Challenges.Phase_02_OOP;
+﻿using System.Text.Json;
+using CodingChallenges.Challenges.Phase_02_OOP;
+using CodingChallenges.Challenges.Phase_03_Practical_Challenges;
 
 public class Program
 {
+    public class PeopleData
+    {
+        public List<People> Person {get; set;} = [];
+    }
+    public class People
+    {
+        public string Name {get; set;} = string.Empty;
+
+        public string Address {get;set;} = string.Empty;
+
+        public string Mobile {get;set;} = string.Empty;
+
+        public string Email {get;set;} = string.Empty;
+    }
+    public string RedactPersonalData(string json)
+    {
+        if(string.IsNullOrWhiteSpace(json))
+        {
+            return "";
+        }
+
+        var deserialized = JsonSerializer.Deserialize<PeopleData>(json);
+
+        if(deserialized is null || deserialized.Person is null)
+        {
+            return "";
+        }
+
+        foreach(var person in deserialized.Person)
+        {
+            person.Name = "";
+            person.Address = "";
+            person.Mobile = "";
+            person.Email = "";
+        }
+
+        return JsonSerializer.Serialize(deserialized);
+
+    }
     public static void Main(string[] args)
     {
         ChallengeSolutionsRunner.Run_Three();
