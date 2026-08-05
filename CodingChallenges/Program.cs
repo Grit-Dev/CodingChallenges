e84e2c2bcb0106fd724e5d772160d7b287c6dd0a
Grit-Dev <paulmcginley16@outlook.com>
2026-08-05 16:51:09 +0100

    feat: Email address redaction challenge complete with tests + plus all other tests for classes

--- CodingChallenges/Program.cs
+++ CodingChallenges/Program.cs
@@ -38,10 +38,10 @@ public class Program
 
         foreach (var person in deserialized.Person)
         {
-            person.Name = "";
-            person.Address = "";
-            person.Mobile = "";
-            person.Email = "";
+            person.Name = NameRedaction(person.Name);
+            person.Address = AddressRedaction(person.Address);
+            person.Mobile = MobileNumberRedaction(person.Mobile);
+            person.Email = EmailAddressRedaction(person.Email);
         }
 
         return JsonSerializer.Serialize(deserialized, options);
@@ -88,14 +88,14 @@ public class Program
 
     public static string AddressRedaction(string address)
     {
-        if(string.IsNullOrWhiteSpace(address))
+        if (string.IsNullOrWhiteSpace(address))
         {
             return "";
         }
 
-        string [] splitString = address.Split([','], StringSplitOptions.RemoveEmptyEntries);
+        string[] splitString = address.Split([','], StringSplitOptions.RemoveEmptyEntries);
 
-        if(splitString.Length < 2)
+        if (splitString.Length < 2)
         {
             return "";
         }
@@ -105,14 +105,14 @@ public class Program
 
     public static string MobileNumberRedaction(string mobileNumber)
     {
-        if(string.IsNullOrWhiteSpace(mobileNumber))
+        if (string.IsNullOrWhiteSpace(mobileNumber))
         {
             return "";
         }
 
         mobileNumber = mobileNumber.Trim();
 
-        if(mobileNumber.Length <= 4)
+        if (mobileNumber.Length <= 4)
         {
             return mobileNumber;
         }
@@ -126,8 +126,54 @@ public class Program
         return asterisks + lastFourCharacters;
     }
 
+    public static string EmailAddressRedaction(string emailAddress)
+    {
+        if (string.IsNullOrWhiteSpace(emailAddress))
+        {
+            return "";
+        }
+
+        emailAddress = emailAddress.Trim();
+
+        int indexOfAt = emailAddress.IndexOf('@');
+
+        if (indexOfAt <= 0)
+        {
+            return "";
+        }
+
+        string asterisks = new('*', indexOfAt);
+
+        string restOfEmail = emailAddress.Substring(indexOfAt);
+
+        return asterisks + restOfEmail;
+    }
+
     public static void Main(string[] args)
     {
+
+        // NameRedaction
+        Console.WriteLine(NameRedaction("Derek,Morgan") == "DM");
+        Console.WriteLine(NameRedaction("Robert,McDowell") == "RD");
+        Console.WriteLine(NameRedaction("Paul") == "P");
+        Console.WriteLine(NameRedaction("") == "");
+
+        // AddressRedaction
+        Console.WriteLine(AddressRedaction("4321 Willow Lane, Edinburgh, EH12 7JQ") == "Edinburgh");
+        Console.WriteLine(AddressRedaction("NoComma") == "");
+        Console.WriteLine(AddressRedaction("") == "");
+
+        // MobileNumberRedaction
+        Console.WriteLine(MobileNumberRedaction("07700900832") == "*******0832");
+        Console.WriteLine(MobileNumberRedaction("1234") == "1234");
+        Console.WriteLine(MobileNumberRedaction("") == "");
+
+        // EmailAddressRedaction
+        Console.WriteLine(EmailAddressRedaction("derek.morgan@example.co.uk") == "************@example.co.uk");
+        Console.WriteLine(EmailAddressRedaction("@example.co.uk") == "");
+        Console.WriteLine(EmailAddressRedaction("invalidemail") == "");
+        Console.WriteLine(EmailAddressRedaction("") == "");
+
         // ChallengeSolutionsRunner.Run_Three();
         // JsonDataSanitizerRunner.Run();
     }
