ffea414745757cf14803493715d7cba59a34072e
Grit-Dev <paulmcginley16@outlook.com>
2026-08-10 11:17:17 +0100

    Challenge 2: Mask Code Except Last Four Rev with Cw tests

--- CodingChallenges/Program.cs
+++ CodingChallenges/Program.cs
@@ -2,19 +2,19 @@
 {
     public static int CountValidScores_Rev(string input)
     {
-        if(string.IsNullOrWhiteSpace(input))
+        if (string.IsNullOrWhiteSpace(input))
         {
             return 0;
         }
 
         int counter = 0;
-        string [] splitString = input.Split(',', StringSplitOptions.RemoveEmptyEntries);
+        string[] splitString = input.Split(',', StringSplitOptions.RemoveEmptyEntries);
 
-        foreach(var str in splitString)
+        foreach (var str in splitString)
         {
-            if(int.TryParse(str.Trim(), out int result))
+            if (int.TryParse(str.Trim(), out int result))
             {
-                if(result >= 0 && result <= 100)
+                if (result >= 0 && result <= 100)
                 {
                     counter++;
                 }
@@ -23,8 +23,37 @@
 
         return counter;
     }
+
+    public static string MaskReferenceExceptLastFour_Rev(string reference)
+    {
+        if (string.IsNullOrWhiteSpace(reference))
+        {
+            return "";
+        }
+
+        if(reference.Length <= 4)
+        {
+            return reference;
+        }
+
+        int characterLeftOver = reference.Length - 4;
+
+        string asterisks = new('*', characterLeftOver);
+
+        string lastFourCharacters = reference.Substring(characterLeftOver);
+
+        return asterisks + lastFourCharacters;
+    }
     public static void Main(string[] args)
     {
+
+        // Mask Reference Except Last Four
+        Console.WriteLine(MaskReferenceExceptLastFour_Rev("ABC1234567") == "******4567");
+        Console.WriteLine(MaskReferenceExceptLastFour_Rev("1234") == "1234");
+        Console.WriteLine(MaskReferenceExceptLastFour_Rev("ABC") == "ABC");
+        Console.WriteLine(MaskReferenceExceptLastFour_Rev("") == "");
+        Console.WriteLine(MaskReferenceExceptLastFour_Rev(" ") == "");
+
         // Count Valid Scores
         Console.WriteLine(CountValidScores_Rev("10, 50, 101, -1, hello, 80") == 3);
         Console.WriteLine(CountValidScores_Rev("0, 100, 55") == 3);
