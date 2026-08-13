91634c877d74a5d0ed5fb92ed5ff117ec5efae45
Paul McGinley <paulmcginley16@outlook.com>
2026-08-13 17:47:34 +0100

    feat: Has balanced parenthesis Revision part two with console.writelines

--- CodingChallenges/Program.cs
+++ CodingChallenges/Program.cs
@@ -1,7 +1,46 @@
 ﻿public class Program
 {
+    public static bool HasBalancedParentheses_RevTwo(string input)
+    {
+        if (string.IsNullOrWhiteSpace(input))
+        {
+            return true;
+        }
+
+        int balance = 0;
+
+        foreach (char character in input)
+        {
+            if (character == '(')
+            {
+                balance++;
+            }
+            else if (character == ')')
+            {
+                balance--;
+
+
+                if (balance < 0)
+                {
+                    return false;
+                }
+            }
+        }
+
+        return balance == 0;
+    }
     public static void Main(string[] args)
     {
+        // Has Balanced Parentheses Rev Two
+        Console.WriteLine(HasBalancedParentheses_RevTwo("(Hello)") == true);
+        Console.WriteLine(HasBalancedParentheses_RevTwo("((Hello))") == true);
+        Console.WriteLine(HasBalancedParentheses_RevTwo("))Hello)") == false);
+        Console.WriteLine(HasBalancedParentheses_RevTwo("((Hello)") == false);
+        Console.WriteLine(HasBalancedParentheses_RevTwo(")hello)") == false);
+        Console.WriteLine(HasBalancedParentheses_RevTwo("Hello)") == false);
+        Console.WriteLine(HasBalancedParentheses_RevTwo("") == true);
+
+
         // ChallengeSolutionsRunner.Run_Three();
         // JsonDataSanitizerRunner.Run();
     }
