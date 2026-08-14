d4d333d600f886f27a5210ed1ebfe71c48a915c9
Grit-Dev <paulmcginley16@outlook.com>
2026-08-14 13:26:30 +0100

    feat: Find Closest Number To Zero revision with Console.Writelines testing

--- CodingChallenges/Program.cs
+++ CodingChallenges/Program.cs
@@ -52,15 +52,54 @@
 
         return strongPasswordCounter;
     }
+
+    public static int? FindClosestNumberToZero_Revision(int[] numbers)
+    {
+        if(numbers is null || numbers.Length == 0)
+        {
+            return null;
+        }
+
+        int indexNumberClosestToZero = numbers[0];
+        int distanceToZero = Math.Abs(numbers[0]);
+
+        foreach(int digit in numbers)
+        {
+            int digitProcessed = Math.Abs(digit);
+
+            if(digitProcessed == distanceToZero && digit > indexNumberClosestToZero)
+            {
+                indexNumberClosestToZero = digit;
+            }
+
+            if(digitProcessed < distanceToZero)
+            {
+                indexNumberClosestToZero = digit;
+                distanceToZero = digitProcessed;
+            }
+        }
+
+        return indexNumberClosestToZero;
+
+    }
+
     public static void Main(string[] args)
     {
+        // Find closest Number To Zero Revision
+        Console.WriteLine(FindClosestNumberToZero_Revision([-5, -2, 3, 2]) == 2);
+        Console.WriteLine(FindClosestNumberToZero_Revision([-10, -4, -2]) == -2);
+        Console.WriteLine(FindClosestNumberToZero_Revision([8, -8]) == 8);
+        Console.WriteLine(FindClosestNumberToZero_Revision([0, 5, -1]) == 0);
+        Console.WriteLine(FindClosestNumberToZero_Revision(null!) == null);
+        Console.WriteLine(FindClosestNumberToZero_Revision([]) == null);
+
         // Count Strong Passwords Revision
-        Console.WriteLine(CountStrongPasswords_Revision("Password1, hello, TEST1234, GoodPass9") == 2);
-        Console.WriteLine(CountStrongPasswords_Revision("abc, 12345678, NoDigitsHere") == 0);
-        Console.WriteLine(CountStrongPasswords_Revision("Aa123456") == 1);
-        Console.WriteLine(CountStrongPasswords_Revision("") == 0);
-        Console.WriteLine(CountStrongPasswords_Revision(" ") == 0);
-        Console.WriteLine(CountStrongPasswords_Revision(null!) == 0);
+        // Console.WriteLine(CountStrongPasswords_Revision("Password1, hello, TEST1234, GoodPass9") == 2);
+        // Console.WriteLine(CountStrongPasswords_Revision("abc, 12345678, NoDigitsHere") == 0);
+        // Console.WriteLine(CountStrongPasswords_Revision("Aa123456") == 1);
+        // Console.WriteLine(CountStrongPasswords_Revision("") == 0);
+        // Console.WriteLine(CountStrongPasswords_Revision(" ") == 0);
+        // Console.WriteLine(CountStrongPasswords_Revision(null!) == 0);
 
         // VaultItemInheritanceRunner.Run();
         // CardShopCompositionChallenges.Run();
