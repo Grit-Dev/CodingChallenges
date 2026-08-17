0ae7370212b51dd7ae2e68ad7308332afc189b3f
Grit-Dev <paulmcginley16@outlook.com>
2026-08-17 14:29:48 +0100

    feat: Challenge 3 - FindFirstNumberExactlyTwice

--- CodingChallenges/Program.cs
+++ CodingChallenges/Program.cs
@@ -59,7 +59,7 @@ public class Program
             {
                 continue;
             }
-            
+
             if (!strTrimmed.StartsWith("ORD-", StringComparison.OrdinalIgnoreCase))
             {
                 continue;
@@ -75,15 +75,52 @@ public class Program
 
         return validCounter;
     }
+
+    public static int? FindFirstNumberRepeatedExactlyTwice(int[] numbers)
+    {
+        if (numbers is null || numbers.Length == 0)
+        {
+            return null;
+        }
+
+        for (int outerIndex = 0; outerIndex <= numbers.Length -1; outerIndex++)
+        {
+            int currentNumber = numbers[outerIndex];
+            int counter = 0;
+
+            for (int innerIndex = 0; innerIndex <= numbers.Length -1; innerIndex++)
+            {
+                if (currentNumber == numbers[innerIndex])
+                {
+                    counter++;
+                }
+            }
+
+            if (counter == 2)
+            {
+                return currentNumber;
+            }
+        }
+
+        return null;
+    }
     public static void Main(string[] args)
     {
+        // Find First Number Repeated Exactly Twice
+        Console.WriteLine(FindFirstNumberRepeatedExactlyTwice([1, 2, 3, 2, 4]) == 2);
+        Console.WriteLine(FindFirstNumberRepeatedExactlyTwice([5, 1, 5, 2, 1]) == 5);
+        Console.WriteLine(FindFirstNumberRepeatedExactlyTwice([7, 7, 7, 1, 1]) == 1);
+        Console.WriteLine(FindFirstNumberRepeatedExactlyTwice([1, 2, 3]) == null);
+        Console.WriteLine(FindFirstNumberRepeatedExactlyTwice(null!) == null);
+        Console.WriteLine(FindFirstNumberRepeatedExactlyTwice([]) == null);
+
         // Count Valid Order References
-        Console.WriteLine(CountValidOrderReferences("ORD-1234, ORD-9999, BAD-1234") == 2);
-        Console.WriteLine(CountValidOrderReferences("ord-1111, ORD-12A4, ORD-12345") == 1);
-        Console.WriteLine(CountValidOrderReferences("hello, ORD-0001") == 1);
-        Console.WriteLine(CountValidOrderReferences("") == 0);
-        Console.WriteLine(CountValidOrderReferences(" ") == 0);
-        Console.WriteLine(CountValidOrderReferences(null!) == 0);
+        // Console.WriteLine(CountValidOrderReferences("ORD-1234, ORD-9999, BAD-1234") == 2);
+        // Console.WriteLine(CountValidOrderReferences("ord-1111, ORD-12A4, ORD-12345") == 1);
+        // Console.WriteLine(CountValidOrderReferences("hello, ORD-0001") == 1);
+        // Console.WriteLine(CountValidOrderReferences("") == 0);
+        // Console.WriteLine(CountValidOrderReferences(" ") == 0);
+        // Console.WriteLine(CountValidOrderReferences(null!) == 0);
 
         // Find Longest Consecutive Run
         // Console.WriteLine(FindLongestConsecutiveRun_One([1, 1, 2, 2, 2, 3]));
