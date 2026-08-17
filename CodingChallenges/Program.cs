21191d56a28e6c077981e0eb0b84e6b8fe03ace2
Grit-Dev <paulmcginley16@outlook.com>
2026-08-17 13:52:46 +0100

    feat: Challenge 1 - Finde Longest Consecutive Run One - Rev - Not the most ideal way but it works

--- CodingChallenges/Program.cs
+++ CodingChallenges/Program.cs
@@ -1,7 +1,56 @@
-﻿public class Program
+﻿using System.Diagnostics.Metrics;
+
+public class Program
 {
+
+    public static int FindLongestConsecutiveRun_One(int[] numbers)
+    {
+        if (numbers is null || numbers.Length == 0)
+        {
+            return 0;
+        }
+
+        int sequenceCounter = 1;
+        int highestCounter = 1;
+        int previousIndexValue = numbers[0];
+
+        for (int outterIndex = 1; outterIndex <= numbers.Length - 1; outterIndex++)
+        {
+            int indexCurrently = numbers[outterIndex];
+
+            if (previousIndexValue != indexCurrently)
+            {
+                previousIndexValue = indexCurrently;
+                sequenceCounter = 1;
+                continue;
+            }
+
+            if (previousIndexValue == indexCurrently)
+            {
+                sequenceCounter++;
+            }
+
+            if (sequenceCounter > highestCounter)
+            {
+                highestCounter = sequenceCounter;
+            }
+
+            previousIndexValue = indexCurrently;
+        }
+
+        return highestCounter;
+    }
     public static void Main(string[] args)
     {
+        // Find Longest Consecutive Run
+        Console.WriteLine(FindLongestConsecutiveRun_One([1, 1, 2, 2, 2, 3]));
+        Console.WriteLine(FindLongestConsecutiveRun_One([5, 5, 5, 5]) == 4);
+        Console.WriteLine(FindLongestConsecutiveRun_One([1, 2, 3, 4]) == 1);
+        Console.WriteLine(FindLongestConsecutiveRun_One([7]) == 1);
+        Console.WriteLine(FindLongestConsecutiveRun_One([1, 1, 2, 1, 1, 1]) == 3);
+        Console.WriteLine(FindLongestConsecutiveRun_One(null!) == 0);
+        Console.WriteLine(FindLongestConsecutiveRun_One([]) == 0);
+
         // VaultItemInheritanceRunner.Run();
         // CardShopCompositionChallenges.Run();
         // ChallengeSolutionsRunner.Run_Four();
