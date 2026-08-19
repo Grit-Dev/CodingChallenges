ffc9c00c1e9b555a5801e297641af98a96a0476b
Grit-Dev <paulmcginley16@outlook.com>
2026-08-19 14:11:06 +0100

    Warm up challenger 1 - Count Negative Odd numbers

--- CodingChallenges/Program.cs
+++ CodingChallenges/Program.cs
@@ -3,9 +3,35 @@ using CodingChallenges.Challenges.Phase_02_OOP.Task_Tracker_Basics;
 
 public class Program
 {
+    public static int CountNegativeOddNumbers(int[] numbers)
+    {
+        if (numbers is null || numbers.Length == 0)
+        {
+            return 0;
+        }
+
+        int counter = 0;
+
+        foreach (int digit in numbers)
+        {
+            if (digit < 0 && digit % 2 != 0)
+            {
+                counter++;
+            }
+        }
+
+        return counter;
+    }
     public static void Main(string[] args)
     {
-        RunnerChecks.Run();
+        //Count Negative Odd Numbers
+        Console.WriteLine(CountNegativeOddNumbers([-1, -2, -3, 4, 5]) == 2);
+        Console.WriteLine(CountNegativeOddNumbers([-10, -11, -13]) == 2);
+        Console.WriteLine(CountNegativeOddNumbers([1, 3, 5]) == 0);
+        Console.WriteLine(CountNegativeOddNumbers(null!) == 0);
+        Console.WriteLine(CountNegativeOddNumbers([]) == 0);
+
+        // RunnerChecks.Run();
         // VaultItemInheritanceRunner.Run();
         // CardShopCompositionChallenges.Run();
         // ChallengeSolutionsRunner.Run_Four();
