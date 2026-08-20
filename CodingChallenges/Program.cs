a09b055a669385eb827e0c29836798a13fcc0a5c
Grit-Dev <paulmcginley16@outlook.com>
2026-08-20 11:38:38 +0100

    Warm up challenge 1 - Count Numbers between 10 and 20 + cw

--- CodingChallenges/Program.cs
+++ CodingChallenges/Program.cs
@@ -1,7 +1,34 @@
 ﻿public class Program
 {
+    public static int CountNumbersBetweenTenAndTwenty(int[] numbers)
+    {
+        if (numbers is null || numbers.Length == 0)
+        {
+            return 0;
+        }
+
+        int count = 0;
+
+        foreach (int digit in numbers)
+        {
+            if (digit >= 10 && digit <= 20)
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
     public static void Main(string[] args)
     {
+        // Count Numbers Between 10 and 20
+        Console.WriteLine(CountNumbersBetweenTenAndTwenty([5, 10, 15, 20, 25]) == 3);
+        Console.WriteLine(CountNumbersBetweenTenAndTwenty([1, 2, 3]) == 0);
+        Console.WriteLine(CountNumbersBetweenTenAndTwenty([10, 20, 30]) == 2);
+        Console.WriteLine(CountNumbersBetweenTenAndTwenty(null!) == 0);
+        Console.WriteLine(CountNumbersBetweenTenAndTwenty([]) == 0);
+
         // RunnerChecks.Run();
         // VaultItemInheritanceRunner.Run();
         // CardShopCompositionChallenges.Run();
