2e67569636f73d58313510de0479a27245603f13
Grit-Dev <paulmcginley16@outlook.com>
2026-08-21 13:36:51 +0100

    feat: Challenge 2: Count Valid Ticket Codes + Cw

--- CodingChallenges/Program.cs
+++ CodingChallenges/Program.cs
@@ -2,16 +2,16 @@
 {
     public static int CountPositiveMultiplesOfThree(int[] numbers)
     {
-        if(numbers is null || numbers.Length == 0)
+        if (numbers is null || numbers.Length == 0)
         {
             return 0;
         }
 
         int counter = 0;
 
-        foreach(int digit in numbers)
+        foreach (int digit in numbers)
         {
-            if(digit > 0 && digit % 3 == 0)
+            if (digit > 0 && digit % 3 == 0)
             {
                 counter++;
             }
@@ -19,14 +19,49 @@
 
         return counter;
     }
+    public static int CountValidTicketCodes(string input)
+    {
+        if(string.IsNullOrWhiteSpace(input))
+        {
+            return 0;
+        }
+
+        int counter = 0;
+        string [] splitString = input.Split([','], StringSplitOptions.RemoveEmptyEntries);
+
+        foreach(string str in splitString)
+        {
+            string strTrimmed = str.Trim();
+
+            if(strTrimmed.Length == 8 && 
+            strTrimmed.StartsWith("TCK-", StringComparison.OrdinalIgnoreCase))
+            {                
+                if(int.TryParse(strTrimmed.Substring(3), out int value))
+                {
+                    counter++;
+                }
+            }
+        }
+
+        return counter;
+    }
     public static void Main(string[] args)
     {
+        // Count Valid Ticket Codes
+        Console.WriteLine(CountValidTicketCodes("TCK-1234, TCK-9999, BAD-1234") == 2);
+        Console.WriteLine(CountValidTicketCodes("tck-1111, TCK-12A4, TCK-12345") == 1);
+        Console.WriteLine(CountValidTicketCodes("hello, TCK-0001") == 1);
+        Console.WriteLine(CountValidTicketCodes("") == 0);
+        Console.WriteLine(CountValidTicketCodes("  ") == 0);
+        Console.WriteLine(CountValidTicketCodes(null!) == 0);
+
+
         // Count Positive Multiples of 3
-        Console.WriteLine(CountPositiveMultiplesOfThree([3, 6, -3, 10, 12]) == 3);
-        Console.WriteLine(CountPositiveMultiplesOfThree([1, 2, 4, 5]) == 0);
-        Console.WriteLine(CountPositiveMultiplesOfThree([0, 3, 9]) == 2);
-        Console.WriteLine(CountPositiveMultiplesOfThree(null!) == 0);
-        Console.WriteLine(CountPositiveMultiplesOfThree([]) == 0);
+        // Console.WriteLine(CountPositiveMultiplesOfThree([3, 6, -3, 10, 12]) == 3);
+        // Console.WriteLine(CountPositiveMultiplesOfThree([1, 2, 4, 5]) == 0);
+        // Console.WriteLine(CountPositiveMultiplesOfThree([0, 3, 9]) == 2);
+        // Console.WriteLine(CountPositiveMultiplesOfThree(null!) == 0);
+        // Console.WriteLine(CountPositiveMultiplesOfThree([]) == 0);
 
         // RunnerChecks.Run();
         // VaultItemInheritanceRunner.Run();
