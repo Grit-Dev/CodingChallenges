75286b0d867e23ccde5c635f4018b1640e23ba93
Grit-Dev <paulmcginley16@outlook.com>
2026-08-24 11:55:58 +0100

    feat: Find Longest Same Word Streak + Cw

--- CodingChallenges/Program.cs
+++ CodingChallenges/Program.cs
@@ -51,16 +51,62 @@ public class Program
 
         return counter;
     }
+    
+    public static int FindLongestSameWordStreak(string input)
+    {
+        if(string.IsNullOrWhiteSpace(input))
+        {
+            return 0;
+        }
+
+        int counter = 1;
+        int highestStreak = 1;
+        string [] splitString = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+        string previousValue = splitString[0].Trim();
+        
+        for(int outerIndex = 1; outerIndex <= splitString.Length -1; outerIndex++)
+        {
+            string strTrimmed = splitString[outerIndex];
+
+            if(strTrimmed.Equals(previousValue, StringComparison.OrdinalIgnoreCase))
+            {
+                counter++;
+            }
+            else
+            {
+                counter = 1;
+            }
+
+            if(counter > highestStreak)
+            {
+                highestStreak = counter;
+            }
+
+            previousValue = strTrimmed;
+        }
+
+        return highestStreak;
+    }
     public static void Main(string[] args)
     {
-        //Count Valid Employee Codes = Stirng Parsing: 
-        Console.WriteLine(CountValidEmployeeCodes("EMP-1234, EMP-9999, BAD-1234") == 2);
-        Console.WriteLine(CountValidEmployeeCodes("emp-0001, EMP-12A4, EMP-12345") == 1);
-        Console.WriteLine(CountValidEmployeeCodes("hello, EMP-7777") == 1);
-        Console.WriteLine(CountValidEmployeeCodes("") == 0);
-        Console.WriteLine(CountValidEmployeeCodes(" ") == 0);
-        Console.WriteLine(CountValidEmployeeCodes(null!) == 0);
-        
+        // Find Longest Same Word Streak
+        Console.WriteLine(FindLongestSameWordStreak("red red blue blue blue red") == 3);
+        Console.WriteLine(FindLongestSameWordStreak("cat cat cat") == 3);
+        Console.WriteLine(FindLongestSameWordStreak("one two three") == 1);
+        Console.WriteLine(FindLongestSameWordStreak("Dog dog DOG cat") == 3);
+        Console.WriteLine(FindLongestSameWordStreak("") == 0);
+        Console.WriteLine(FindLongestSameWordStreak(" ") == 0);
+        Console.WriteLine(FindLongestSameWordStreak(null!) == 0);
+
+        //Count Valid Employee Codes = String Parsing
+        // Console.WriteLine(CountValidEmployeeCodes("EMP-1234, EMP-9999, BAD-1234") == 2);
+        // Console.WriteLine(CountValidEmployeeCodes("emp-0001, EMP-12A4, EMP-12345") == 1);
+        // Console.WriteLine(CountValidEmployeeCodes("hello, EMP-7777") == 1);
+        // Console.WriteLine(CountValidEmployeeCodes("") == 0);
+        // Console.WriteLine(CountValidEmployeeCodes(" ") == 0);
+        // Console.WriteLine(CountValidEmployeeCodes(null!) == 0);
+
         // Count Numbers Ending in Five
         // Console.WriteLine(CountNumbersEndingInFive([5, 15, 20, -25, 100 ]) == 3);
         // Console.WriteLine(CountNumbersEndingInFive([ 1, 2, 3, 4 ]) == 0);
