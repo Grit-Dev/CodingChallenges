6480e76e357b7e0996ed08c4f70a905090a1b5ea
Grit-Dev <paulmcginley16@outlook.com>
2026-08-25 10:53:45 +0100

    feat: Count Odd Numbers At Odd Indexes + CW

--- CodingChallenges/Program.cs
+++ CodingChallenges/Program.cs
@@ -1,7 +1,33 @@
 ﻿public class Program
 {
+    public static int CountOddNumbersAtOddIndexes(int [] numbers)
+    {
+        if(numbers is null || numbers.Length == 0)
+        {
+            return 0;
+        }
+
+        int counter = 0;
+
+        for(int outerIndex = 1; outerIndex <= numbers.Length -1; outerIndex += 2)
+        {
+            if(numbers[outerIndex] % 2 != 0)
+            {
+                counter++;
+            }
+        }
+
+        return counter;
+    }
     public static void Main(string[] args)
     {
+        // Count Odd Numbers At Odd Indexes
+        Console.WriteLine(CountOddNumbersAtOddIndexes([10, 3, 8, 7, 6]) == 2);
+        Console.WriteLine(CountOddNumbersAtOddIndexes([1, 2, 3, 4]) == 0);
+        Console.WriteLine(CountOddNumbersAtOddIndexes([5, 9, 2, 11]) == 2);
+        Console.WriteLine(CountOddNumbersAtOddIndexes(null!) == 0);
+        Console.WriteLine(CountOddNumbersAtOddIndexes([]) == 0);
+
         // RunnerChecks.Run();
         // VaultItemInheritanceRunner.Run();
         // CardShopCompositionChallenges.Run();
