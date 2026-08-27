e76d2a70eee9e3af38caa226a77e805baee38d3e
Grit-Dev <paulmcginley16@outlook.com>
2026-08-27 09:34:28 +0100

    feat: warm up challenge: count negative multiples of three + CW

--- CodingChallenges/Program.cs
+++ CodingChallenges/Program.cs
@@ -1,7 +1,33 @@
 ﻿public class Program
 {
+    public static int CountNegativeMultiplesOfThreeWarmUp(int[] numbers)
+    {
+        if (numbers is null || numbers.Length == 0)
+        {
+            return 0;
+        }
+
+        int counter = 0;
+
+        foreach(int digit in numbers)
+        {
+            if( digit < 0 && digit % 3 == 0)
+            {
+                counter++;        
+            }
+        }
+
+        return counter;
+    }
     public static void Main(string[] args)
     {
+        // Count Negative Multiples Of Three
+        Console.WriteLine(CountNegativeMultiplesOfThreeWarmUp([-3, -6, 3, 9, -10]) == 2);
+        Console.WriteLine(CountNegativeMultiplesOfThreeWarmUp([-1, -2, -4]) == 0);
+        Console.WriteLine(CountNegativeMultiplesOfThreeWarmUp([-9, 0, -12]) == 2);
+        Console.WriteLine(CountNegativeMultiplesOfThreeWarmUp(null!) == 0);
+        Console.WriteLine(CountNegativeMultiplesOfThreeWarmUp([]) == 0);
+
         // RunnerChecks.Run();
         // VaultItemInheritanceRunner.Run();
         // CardShopCompositionChallenges.Run();
