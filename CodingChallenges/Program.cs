738c11f4a8f43002e31be2951ff9288564802f92
Grit-Dev <paulmcginley16@outlook.com>
2026-08-27 09:42:08 +0100

    feat: Count Valid Case Codes + CW = Challenge 2

--- CodingChallenges/Program.cs
+++ CodingChallenges/Program.cs
@@ -9,11 +9,36 @@
 
         int counter = 0;
 
-        foreach(int digit in numbers)
+        foreach (int digit in numbers)
         {
-            if( digit < 0 && digit % 3 == 0)
+            if (digit < 0 && digit % 3 == 0)
             {
-                counter++;        
+                counter++;
+            }
+        }
+
+        return counter;
+    }
+
+    public static int CountValidCaseCodes(string input)
+    {
+        if(string.IsNullOrWhiteSpace(input))
+        {
+            return 0;
+        }
+
+        int counter = 0;
+        string [] splitString = input.Split([','], StringSplitOptions.RemoveEmptyEntries);
+
+        foreach(string str in splitString)
+        {
+            string strTrimmed = str.Trim();
+
+            if(strTrimmed.Length == 9 &&
+            strTrimmed.StartsWith("CASE-", StringComparison.OrdinalIgnoreCase) &&
+            int.TryParse(strTrimmed.Substring(5), out _))
+            {
+                counter++;
             }
         }
 
@@ -21,6 +46,15 @@
     }
     public static void Main(string[] args)
     {
+        // Count Valid Case Codes
+        Console.WriteLine(CountValidCaseCodes("CASE-1234, CASE-9999, BAD-1234") == 2);
+        Console.WriteLine(CountValidCaseCodes("case-0001, CASE-12A4, CASE-12345") == 1);
+        Console.WriteLine(CountValidCaseCodes("hello, CASE-7777") == 1);
+        Console.WriteLine(CountValidCaseCodes("") == 0);
+        Console.WriteLine(CountValidCaseCodes(" ") == 0);
+        Console.WriteLine(CountValidCaseCodes(null!) == 0);
+
+
         // Count Negative Multiples Of Three
         Console.WriteLine(CountNegativeMultiplesOfThreeWarmUp([-3, -6, 3, 9, -10]) == 2);
         Console.WriteLine(CountNegativeMultiplesOfThreeWarmUp([-1, -2, -4]) == 0);
