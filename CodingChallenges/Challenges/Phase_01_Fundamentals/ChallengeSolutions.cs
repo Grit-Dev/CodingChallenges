98e1b9b281351bbd70906df903fd23648d81850c
Grit-Dev <paulmcginley16@outlook.com>
2026-09-01 10:34:43 +0100

    refactor: refactoring todays challenges into the the correct file classes within Challenge Folder

--- CodingChallenges/Challenges/Phase_01_Fundamentals/ChallengeSolutions.cs
+++ CodingChallenges/Challenges/Phase_01_Fundamentals/ChallengeSolutions.cs
@@ -6,6 +6,159 @@ namespace CodingChallenges.Challenges.Phase_02_OOP
 {
     public class ChallengeSolutions
     {
+        public static int CountNumbersOutsideRange(int[] numbers, int min, int max)
+        {
+            if (numbers is null || numbers.Length == 0)
+            {
+                return 0;
+            }
+
+            int counter = 0;
+
+            foreach (int digit in numbers)
+            {
+                if (digit < min || digit > max)
+                {
+                    counter++;
+                }
+            }
+
+            return counter;
+        }
+
+        public static int[] ReplaceNegativesWithZero(int[] numbers)
+        {
+            if (numbers is null || numbers.Length == 0)
+            {
+                return [];
+            }
+
+            List<int> newList = [];
+
+            foreach (int digit in numbers)
+            {
+                if (digit < 0)
+                {
+                    newList.Add(0);
+                }
+                else
+                {
+                    newList.Add(digit);
+                }
+
+
+            }
+
+            return newList.ToArray();
+        }
+
+        public static string RemoveWordsShorterThanThree(string input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                return "";
+            }
+
+            var newStringBuild = new StringBuilder();
+
+            string[] stringSplit = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string str in stringSplit)
+            {
+                if (str.Length >= 3)
+                {
+                    newStringBuild.Append($"{str} ");
+                }
+            }
+
+            return newStringBuild.ToString().Trim();
+
+        }
+
+        public static bool HasPairWithTargetSum(int[] numbers, int target)
+        {
+            if (numbers is null || numbers.Length < 2)
+            {
+                return false;
+            }
+
+            for (int outerIndex = 0; outerIndex <= numbers.Length - 1; outerIndex++)
+            {
+                for (int innerIndex = outerIndex + 1; innerIndex <= numbers.Length - 1; innerIndex++)
+                {
+                    if (numbers[outerIndex] + numbers[innerIndex] == target)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        public static string FindFirstRepeatedWord(string input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                return "";
+            }
+
+            var newDict = new Dictionary<string, int>();
+
+            string[] splitString = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string str in splitString)
+            {
+                string strLowered = str.ToLower();
+
+                if (newDict.ContainsKey(strLowered))
+                {
+                    return str;
+                }
+                else
+                {
+                    newDict[strLowered] = 1;
+                }
+            }
+
+            return "";
+        }
+
+        public static int[] GetPassingScoresWithLinq(int[] scores) =>
+        scores?.Where(s => s >= 50).ToArray() ?? [];
+
+        public static string FindFirstWordWithTwoVowels(string input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                return "";
+            }
+
+            List<char> vowelList = ['a', 'e', 'i', 'o', 'u'];
+
+            string[] splitString = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string str in splitString)
+            {
+                int counter = 0;
+                string strLowered = str.ToLower();
+
+                foreach (char character in strLowered)
+                {
+                    if (vowelList.Contains(character))
+                    {
+                        counter++;
+                    }
+
+                    if (counter == 2)
+                    {
+                        return str;
+                    }
+                }
+            }
+
+            return "";
+        }
         public static int CountNegativeEvenNumbersWarmUp(int[] numbers)
         {
             if (numbers is null || numbers.Length == 0)
@@ -3421,43 +3574,6 @@ namespace CodingChallenges.Challenges.Phase_02_OOP
             }
 
             return newStringtobeReturned.ToString().Trim();
-        }
-
-        public static string FindFirstRepeatedWord(string pInputValue)
-        {
-            if (string.IsNullOrEmpty(pInputValue))
-            {
-                return "";
-            }
-
-            List<string> newListCheckerLowered = [];
-            List<string> newListForStringSplitOriginal = [];
-
-            string[] stringSplit = pInputValue.Split(' ', StringSplitOptions.RemoveEmptyEntries);
-
-            for (int outterIndex = 0; outterIndex <= stringSplit.Length - 1; outterIndex++)
-            {
-                string wordLowered = stringSplit[outterIndex].ToLower();
-
-                if (!newListCheckerLowered.Contains(wordLowered))
-                {
-                    newListCheckerLowered.Add(wordLowered);
-                    newListForStringSplitOriginal.Add(stringSplit[outterIndex]);
-                }
-                else
-                {
-                    for (int counter = 0; counter <= newListCheckerLowered.Count - 1; counter++)
-                    {
-                        if (wordLowered == newListCheckerLowered[counter])
-                        {
-                            return newListForStringSplitOriginal[counter];
-                        }
-                    }
-
-                }
-            }
-
-            return "";
         }
 
         public static string FindFirstWordWithDuplicateLetters(string pInputValue)
