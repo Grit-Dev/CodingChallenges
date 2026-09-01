c1f5a4e72d5f85e597dbbb5a96bebfb2715af2a0
Grit-Dev <paulmcginley16@outlook.com>
2026-09-01 09:22:49 +0100

    feat: Remove Words Shorter Than Three + cw

--- CodingChallenges/Program.cs
+++ CodingChallenges/Program.cs
@@ -1,4 +1,6 @@
-﻿public class Program
+﻿using System.Text;
+
+public class Program
 {
     public static int CountNumbersOutsideRange(int[] numbers, int min, int max)
     {
@@ -22,16 +24,16 @@
 
     public static int[] ReplaceNegativesWithZero(int[] numbers)
     {
-        if(numbers is null || numbers.Length == 0)
+        if (numbers is null || numbers.Length == 0)
         {
             return [];
         }
 
         List<int> newList = [];
 
-        foreach(int digit in numbers)
+        foreach (int digit in numbers)
         {
-            if(digit < 0)
+            if (digit < 0)
             {
                 newList.Add(0);
             }
@@ -46,22 +48,53 @@
         return newList.ToArray();
     }
 
+    public static string RemoveWordsShorterThanThree(string input)
+    {
+        if(string.IsNullOrWhiteSpace(input))
+        {
+            return "";
+        }
+
+        var newStringBuild = new StringBuilder();
+
+        string[] stringSplit = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+        foreach(string str in stringSplit)
+        {
+            if(str.Length >= 3)
+            {
+                newStringBuild.Append($"{str} ");
+            }
+        }
+
+        return newStringBuild.ToString().Trim();
+
+    }
+
     public static void Main(string[] args)
     {
+        // Remove Words Shorter Than Three
+        Console.WriteLine(RemoveWordsShorterThanThree("hi paul is coding today") == "paul coding today");
+        Console.WriteLine(RemoveWordsShorterThanThree("a big red cat") == "big red cat");
+        Console.WriteLine(RemoveWordsShorterThanThree("to be or not") == "not");
+        Console.WriteLine(RemoveWordsShorterThanThree("") == "");
+        Console.WriteLine(RemoveWordsShorterThanThree(" ") == "");
+        Console.WriteLine(RemoveWordsShorterThanThree(null!) == "");
+
         // Replace Negatives With Zero
-        int[] replacedOne = ReplaceNegativesWithZero(new int[] { 1, -2, 3, -4 });
-        Console.WriteLine(replacedOne[0] == 1);
-        Console.WriteLine(replacedOne[1] == 0);
-        Console.WriteLine(replacedOne[2] == 3);
-        Console.WriteLine(replacedOne[3] == 0);
+        // int[] replacedOne = ReplaceNegativesWithZero(new int[] { 1, -2, 3, -4 });
+        // Console.WriteLine(replacedOne[0] == 1);
+        // Console.WriteLine(replacedOne[1] == 0);
+        // Console.WriteLine(replacedOne[2] == 3);
+        // Console.WriteLine(replacedOne[3] == 0);
 
-        int[] replacedTwo = ReplaceNegativesWithZero(new int[] { -1, -2, -3 });
-        Console.WriteLine(replacedTwo[0] == 0);
-        Console.WriteLine(replacedTwo[1] == 0);
-        Console.WriteLine(replacedTwo[2] == 0);
+        // int[] replacedTwo = ReplaceNegativesWithZero(new int[] { -1, -2, -3 });
+        // Console.WriteLine(replacedTwo[0] == 0);
+        // Console.WriteLine(replacedTwo[1] == 0);
+        // Console.WriteLine(replacedTwo[2] == 0);
 
-        Console.WriteLine(ReplaceNegativesWithZero(null).Length == 0);
-        Console.WriteLine(ReplaceNegativesWithZero([]).Length == 0);
+        // Console.WriteLine(ReplaceNegativesWithZero(null).Length == 0);
+        // Console.WriteLine(ReplaceNegativesWithZero([]).Length == 0);
 
         // Warm Up: Count Numbers Outside Range
         // Console.WriteLine(CountNumbersOutsideRange([1, 5, 10, 15, 20], 5, 15) == 2);
