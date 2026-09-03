9d5b8d83316a0a806dcabdbf5c845e77628d0b81
Grit-Dev <paulmcginley16@outlook.com>
2026-09-03 08:28:51 +0100

    feat: reverse each word

--- CodingChallenges/Program.cs
+++ CodingChallenges/Program.cs
@@ -1,24 +1,25 @@
 ﻿
+using System.Text;
+
 public class Program
 {
     public static int CountNumbersDivisibleByThreeOrFive(int[] numbers)
     {
-        if(numbers is null || numbers.Length == 0)
+        if (numbers is null || numbers.Length == 0)
         {
             return 0;
         }
 
         int counter = 0;
 
-        foreach(int digit in numbers)
+        foreach (int digit in numbers)
         {
-            if(digit == 0)
+            if (digit == 0)
             {
                 continue;
             }
 
-            if((digit % 3 == 0 && digit % 5 == 0) || 
-            digit % 3 == 0 || digit % 5 == 0)
+            if (digit % 3 == 0 || digit % 5 == 0)
             {
                 counter++;
             }
@@ -26,14 +27,47 @@ public class Program
 
         return counter;
     }
+
+    public static string ReverseEachWord(string input)
+    {
+        if(string.IsNullOrWhiteSpace(input))
+        {
+            return "";
+        }
+
+        var strBuilder = new StringBuilder();
+        string[] stringSplit = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+        foreach(string str in stringSplit)
+        {
+            for(int innerIndex = str.Length -1; innerIndex >= 0; innerIndex--)
+            {
+                strBuilder.Append(str[innerIndex]);
+            }
+
+            strBuilder.Append(' ');
+        }
+
+        return strBuilder.ToString().Trim();
+
+    }
     public static void Main(string[] args)
     {
+        // Reverse Each Word
+        Console.WriteLine(ReverseEachWord("hello world") == "olleh dlrow");
+        Console.WriteLine(ReverseEachWord("Paul Codes") == "luaP sedoC");
+        Console.WriteLine(ReverseEachWord(" one two ") == "eno owt");
+        Console.WriteLine(ReverseEachWord("a") == "a");
+        Console.WriteLine(ReverseEachWord("") == "");
+        Console.WriteLine(ReverseEachWord(" ") == "");
+        Console.WriteLine(ReverseEachWord(null!) == "");
+
         // Warm Up: Count Numbers Divisible By Three Or Five
-        Console.WriteLine(CountNumbersDivisibleByThreeOrFive([3, 5, 15, 7, 10]) == 4);
-        Console.WriteLine(CountNumbersDivisibleByThreeOrFive([1, 2, 4, 8]) == 0);
-        Console.WriteLine(CountNumbersDivisibleByThreeOrFive([0, 30, -5]) == 2);
-        Console.WriteLine(CountNumbersDivisibleByThreeOrFive(null!) == 0);
-        Console.WriteLine(CountNumbersDivisibleByThreeOrFive([]) == 0);
+        // Console.WriteLine(CountNumbersDivisibleByThreeOrFive([3, 5, 15, 7, 10]) == 4);
+        // Console.WriteLine(CountNumbersDivisibleByThreeOrFive([1, 2, 4, 8]) == 0);
+        // Console.WriteLine(CountNumbersDivisibleByThreeOrFive([0, 30, -5]) == 2);
+        // Console.WriteLine(CountNumbersDivisibleByThreeOrFive(null!) == 0);
+        // Console.WriteLine(CountNumbersDivisibleByThreeOrFive([]) == 0);
 
         // ChallengeSolutionsRunner.Run_Four_Dictionary_Work()
         // RunnerChecks.Run();
