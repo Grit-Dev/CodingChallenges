2d939d53f31b9e5488a5e2505c9a01d047da2607
Grit-Dev <paulmcginley16@outlook.com>
2026-09-07 13:27:06 +0100

    feat: Find Largest Jump Between Adjacent Numbers

--- CodingChallenges/Program.cs
+++ CodingChallenges/Program.cs
@@ -42,16 +42,48 @@ public class Program
 
         return input[0] + new string('*', input.Length - 2) + input[input.Length - 1];
     }
+
+    public static int? FindLargestJumpBetweenAdjacentNumbers(int[] numbers)
+    {
+        if (numbers is null || numbers.Length <= 2)
+        {
+            return null;
+        }
+
+        int largestDistanceFound = Math.Abs(numbers[0] - numbers[1]);
+
+        for (int outerIndex = 1; outerIndex <= numbers.Length - 1; outerIndex++)
+        {
+
+            int distanceCounter =  Math.Abs(numbers[outerIndex -1] - numbers[outerIndex]);
+
+            if(distanceCounter > largestDistanceFound)
+            {
+                largestDistanceFound = distanceCounter;
+            }
+        }
+
+        return largestDistanceFound;
+    }
     public static void Main(string[] args)
     {
+
+        // Find Largest Jump Between Adjacent Numbers. 
+        Console.WriteLine(FindLargestJumpBetweenAdjacentNumbers([3, 10, 6, 20]) == 14);
+        Console.WriteLine(FindLargestJumpBetweenAdjacentNumbers([5, 5, 5]) == 0);
+        Console.WriteLine(FindLargestJumpBetweenAdjacentNumbers([-5, 5, -10]) == 15);
+        Console.WriteLine(FindLargestJumpBetweenAdjacentNumbers([7]) == null);
+        Console.WriteLine(FindLargestJumpBetweenAdjacentNumbers(null!) == null);
+        Console.WriteLine(FindLargestJumpBetweenAdjacentNumbers([]) == null);
+
         //Mask Middle Character
-        Console.WriteLine(MaskMiddleCharacters("Paul") == "P**l");
-        Console.WriteLine(MaskMiddleCharacters("coding") == "c****g");
-        Console.WriteLine(MaskMiddleCharacters("ab") == "ab");
-        Console.WriteLine(MaskMiddleCharacters("a") == "a");
-        Console.WriteLine(MaskMiddleCharacters("") == "");
-        Console.WriteLine(MaskMiddleCharacters(" ") == "");
-        Console.WriteLine(MaskMiddleCharacters(null!) == "");
+        // Console.WriteLine(MaskMiddleCharacters("Paul") == "P**l");
+        // Console.WriteLine(MaskMiddleCharacters("coding") == "c****g");
+        // Console.WriteLine(MaskMiddleCharacters("ab") == "ab");
+        // Console.WriteLine(MaskMiddleCharacters("a") == "a");
+        // Console.WriteLine(MaskMiddleCharacters("") == "");
+        // Console.WriteLine(MaskMiddleCharacters(" ") == "");
+        // Console.WriteLine(MaskMiddleCharacters(null!) == "");
 
         // Count Numbers With Same Parity As Their Index
         // Console.WriteLine(CountNumbersWithSameParityAsTheirIndex([2, 3, 4, 7]) == 4);
