ba1c62cf016fa197e762865629430c9d2acef89c
Grit-Dev <paulmcginley16@outlook.com>
2026-09-07 12:11:14 +0100

    feat: mast middle characters + cw

--- CodingChallenges/Program.cs
+++ CodingChallenges/Program.cs
@@ -1,38 +1,64 @@
 ﻿
+using System.Text;
+
 public class Program
 {
     public static int CountNumbersWithSameParityAsTheirIndex(int[] numbers)
     {
-        if(numbers is null || numbers.Length == 0)
+        if (numbers is null || numbers.Length == 0)
         {
             return 0;
         }
 
         int counter = 0;
 
-        for(int outerIndex = 0; outerIndex <= numbers.Length -1; outerIndex++)
+        for (int outerIndex = 0; outerIndex <= numbers.Length - 1; outerIndex++)
         {
-            if(numbers[outerIndex] % 2 == 0 && outerIndex % 2 == 0)
+            if (numbers[outerIndex] % 2 == 0 && outerIndex % 2 == 0)
             {
-               counter++;
+                counter++;
             }
             else if (numbers[outerIndex] % 2 != 0 && outerIndex % 2 != 0)
             {
                 counter++;
             }
-            
+
         }
 
         return counter;
     }
+
+    public static string MaskMiddleCharacters(string input)
+    {
+        if (string.IsNullOrWhiteSpace(input))
+        {
+            return "";
+        }
+
+        if (input.Length <= 2)
+        {
+            return input;
+        }
+
+        return input[0] + new string('*', input.Length - 2) + input[input.Length - 1];
+    }
     public static void Main(string[] args)
     {
+        //Mask Middle Character
+        Console.WriteLine(MaskMiddleCharacters("Paul") == "P**l");
+        Console.WriteLine(MaskMiddleCharacters("coding") == "c****g");
+        Console.WriteLine(MaskMiddleCharacters("ab") == "ab");
+        Console.WriteLine(MaskMiddleCharacters("a") == "a");
+        Console.WriteLine(MaskMiddleCharacters("") == "");
+        Console.WriteLine(MaskMiddleCharacters(" ") == "");
+        Console.WriteLine(MaskMiddleCharacters(null!) == "");
+
         // Count Numbers With Same Parity As Their Index
-        Console.WriteLine(CountNumbersWithSameParityAsTheirIndex([2, 3, 4, 7]) == 4);
-        Console.WriteLine(CountNumbersWithSameParityAsTheirIndex([1, 2, 3, 4]) == 0);
-        Console.WriteLine(CountNumbersWithSameParityAsTheirIndex([0, 5, 8, 10]) == 3);
-        Console.WriteLine(CountNumbersWithSameParityAsTheirIndex(null!) == 0);
-        Console.WriteLine(CountNumbersWithSameParityAsTheirIndex([]) == 0);
+        // Console.WriteLine(CountNumbersWithSameParityAsTheirIndex([2, 3, 4, 7]) == 4);
+        // Console.WriteLine(CountNumbersWithSameParityAsTheirIndex([1, 2, 3, 4]) == 0);
+        // Console.WriteLine(CountNumbersWithSameParityAsTheirIndex([0, 5, 8, 10]) == 3);
+        // Console.WriteLine(CountNumbersWithSameParityAsTheirIndex(null!) == 0);
+        // Console.WriteLine(CountNumbersWithSameParityAsTheirIndex([]) == 0);
 
         // ChallengeSolutionsRunner.Run_Four_Dictionary_Work()
         // RunnerChecks.Run();
