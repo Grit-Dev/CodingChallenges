ad5c9f02e7c07eb76d05111854c113423061a408
Grit-Dev <paulmcginley16@outlook.com>
2026-09-07 11:52:55 +0100

    feat: warm up Count Numbers With Same Parity As their Index

--- CodingChallenges/Program.cs
+++ CodingChallenges/Program.cs
@@ -1,8 +1,39 @@
 ﻿
 public class Program
 {
+    public static int CountNumbersWithSameParityAsTheirIndex(int[] numbers)
+    {
+        if(numbers is null || numbers.Length == 0)
+        {
+            return 0;
+        }
+
+        int counter = 0;
+
+        for(int outerIndex = 0; outerIndex <= numbers.Length -1; outerIndex++)
+        {
+            if(numbers[outerIndex] % 2 == 0 && outerIndex % 2 == 0)
+            {
+               counter++;
+            }
+            else if (numbers[outerIndex] % 2 != 0 && outerIndex % 2 != 0)
+            {
+                counter++;
+            }
+            
+        }
+
+        return counter;
+    }
     public static void Main(string[] args)
     {
+        // Count Numbers With Same Parity As Their Index
+        Console.WriteLine(CountNumbersWithSameParityAsTheirIndex([2, 3, 4, 7]) == 4);
+        Console.WriteLine(CountNumbersWithSameParityAsTheirIndex([1, 2, 3, 4]) == 0);
+        Console.WriteLine(CountNumbersWithSameParityAsTheirIndex([0, 5, 8, 10]) == 3);
+        Console.WriteLine(CountNumbersWithSameParityAsTheirIndex(null!) == 0);
+        Console.WriteLine(CountNumbersWithSameParityAsTheirIndex([]) == 0);
+
         // ChallengeSolutionsRunner.Run_Four_Dictionary_Work()
         // RunnerChecks.Run();
         // VaultItemInheritanceRunner.Run();
