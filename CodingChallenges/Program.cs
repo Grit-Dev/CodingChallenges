a645fc21d6f75a83f05f080f22bd17851d41bc89
Grit-Dev <paulmcginley16@outlook.com>
2026-09-08 09:12:13 +0100

    feat: Count Numbers between two values - warm up + cw

--- CodingChallenges/Program.cs
+++ CodingChallenges/Program.cs
@@ -1,8 +1,35 @@
 ﻿
 public class Program
 {
+    public static int CountNumbersBetweenTwoValues(int[] numbers, int min, int max)
+    {
+        if(numbers is null || numbers.Length == 0 || min > max)
+        {
+            return 0;
+        }
+
+        int counter = 0;
+
+        foreach(int number in numbers)
+        {
+            if(number >= min && number <= max)
+            {
+                counter++;
+            }
+        }
+
+        return counter;
+    }
     public static void Main(string[] args)
     {
+        // Count Numbers Between Two Values
+        Console.WriteLine(CountNumbersBetweenTwoValues([1, 5, 10, 15, 20], 5, 15) == 3);
+        Console.WriteLine(CountNumbersBetweenTwoValues([5, 10, 15], 6, 14) == 1);
+        Console.WriteLine(CountNumbersBetweenTwoValues([-5, 0, 5], -5, 0) == 2);
+        Console.WriteLine(CountNumbersBetweenTwoValues([1, 2, 3], 10, 5) == 0);
+        Console.WriteLine(CountNumbersBetweenTwoValues(null!, 0, 10) == 0);
+        Console.WriteLine(CountNumbersBetweenTwoValues([], 0, 10) == 0);
+
         // ChallengeSolutionsRunner.Run_Four_Dictionary_Work()
         // RunnerChecks.Run();
         // VaultItemInheritanceRunner.Run();
