70de6042830d9d85f81e6a9c3d8a047c8fafc005
Grit-Dev <paulmcginley16@outlook.com>
2026-09-09 10:10:40 +0100

    feat: Find Smalles Positive Number + CW

--- CodingChallenges/Program.cs
+++ CodingChallenges/Program.cs
@@ -56,29 +56,58 @@ public class Program
         int total = 0;
         List<int> newList = [];
 
-        foreach(int number in numbers)
+        foreach (int number in numbers)
         {
             newList.Add(total += number);
         }
 
         return newList.ToArray();
     }
+
+    public static int? FindSmallestPositiveNumber(int[] numbers)
+    {
+        if(numbers is null || numbers.Length == 0)
+        {
+            return null;
+        }
+
+        int? smallestNumberSoFar = null;
+
+        foreach(int number in numbers)
+        {
+            if(number > 0 && (number < smallestNumberSoFar || smallestNumberSoFar is null))
+            {
+                smallestNumberSoFar = number;
+            }
+        }
+
+        return smallestNumberSoFar;
+    }
     public static void Main(string[] args)
     {
-        // Calculate Running Totals
-        int[] totalsOne = CalculateRunningTotals([2, 4, 5]);
-        Console.WriteLine(totalsOne.Length == 3);
-        Console.WriteLine(totalsOne[0] == 2);
-        Console.WriteLine(totalsOne[1] == 6);
-        Console.WriteLine(totalsOne[2] == 11);
+        // Find Smallest Positive Number
+        Console.WriteLine(FindSmallestPositiveNumber([5, 2, -1, 10]) == 2);
+        Console.WriteLine(FindSmallestPositiveNumber([-5, 0, -2]) == null);
+        Console.WriteLine(FindSmallestPositiveNumber([7]) == 7);
+        Console.WriteLine(FindSmallestPositiveNumber([10, 3, 4, 1]) == 1);
+        Console.WriteLine(FindSmallestPositiveNumber(null!) == null);
+        Console.WriteLine(FindSmallestPositiveNumber([]) == null);
 
-        int[] totalsTwo = CalculateRunningTotals([10, -3, 2]);
-        Console.WriteLine(totalsTwo.Length == 3);
-        Console.WriteLine(totalsTwo[0] == 10);
-        Console.WriteLine(totalsTwo[1] == 7);
-        Console.WriteLine(totalsTwo[2] == 9);
-        Console.WriteLine(CalculateRunningTotals(null!).Length == 0);
-        Console.WriteLine(CalculateRunningTotals([]).Length == 0);
+        // Calculate Running Totals
+        // int[] totalsOne = CalculateRunningTotals([2, 4, 5]);
+        // Console.WriteLine(totalsOne.Length == 3);
+        // Console.WriteLine(totalsOne[0] == 2);
+        // Console.WriteLine(totalsOne[1] == 6);
+        // Console.WriteLine(totalsOne[2] == 11);
+
+        // int[] totalsTwo = CalculateRunningTotals([10, -3, 2]);
+        // Console.WriteLine(totalsTwo.Length == 3);
+        // Console.WriteLine(totalsTwo[0] == 10);
+        // Console.WriteLine(totalsTwo[1] == 7);
+        // Console.WriteLine(totalsTwo[2] == 9);
+        // Console.WriteLine(CalculateRunningTotals(null!).Length == 0);
+        // Console.WriteLine(CalculateRunningTotals([]).Length == 0);
+        // Console.WriteLine(CalculateRunningTotals([]).Length == 0);
 
         // Clean Extra Spaces Between Words
         // Console.WriteLine(CleanExtraSpacesBetweenWords("  Paul   is  coding  ") == "Paul is coding");
