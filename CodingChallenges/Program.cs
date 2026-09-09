2405f76f727ec13e079364973918b5fc53bd6ded
Grit-Dev <paulmcginley16@outlook.com>
2026-09-09 08:48:15 +0100

    feat: clean extra spaces between words + cw

--- CodingChallenges/Program.cs
+++ CodingChallenges/Program.cs
@@ -1,4 +1,6 @@
-﻿public class Program
+﻿using System.Text;
+
+public class Program
 {
     public static int CountEvenNumbersAtEvenIndexes(int[] numbers)
     {
@@ -9,7 +11,7 @@
 
         int counter = 0;
 
-        for (int outerIndex = 0; outerIndex <= numbers.Length -1; outerIndex += 2)
+        for (int outerIndex = 0; outerIndex <= numbers.Length - 1; outerIndex += 2)
         {
             if (numbers[outerIndex] % 2 == 0)
             {
@@ -19,14 +21,46 @@
 
         return counter;
     }
+
+    public static string CleanExtraSpacesBetweenWords(string input)
+    {
+        if (string.IsNullOrWhiteSpace(input))
+        {
+            return "";
+        }
+
+        var strBuilder = new StringBuilder();
+
+        string[] stringSplit = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+        foreach (string str in stringSplit)
+        {
+
+            if (strBuilder.Length > 0)
+            {
+                strBuilder.Append(' ');
+            }
+            strBuilder.Append(str);
+        }
+
+        return strBuilder.ToString();
+    }
     public static void Main(string[] args)
     {
+        // Clean Extra Spaces Between Words
+        Console.WriteLine(CleanExtraSpacesBetweenWords("  Paul   is  coding  ") == "Paul is coding");
+        Console.WriteLine(CleanExtraSpacesBetweenWords("hello     world") == "hello world");
+        Console.WriteLine(CleanExtraSpacesBetweenWords("one") == "one");
+        Console.WriteLine(CleanExtraSpacesBetweenWords("") == "");
+        Console.WriteLine(CleanExtraSpacesBetweenWords(" ") == "");
+        Console.WriteLine(CleanExtraSpacesBetweenWords(null!) == "");
+
         // Count Even Numbers At Even Indexes
-        Console.WriteLine(CountEvenNumbersAtEvenIndexes([2, 3, 4, 5, 6]) == 3);
-        Console.WriteLine(CountEvenNumbersAtEvenIndexes([1, 2, 3, 4]) == 0);
-        Console.WriteLine(CountEvenNumbersAtEvenIndexes([0, 1, 8, 3]) == 2);
-        Console.WriteLine(CountEvenNumbersAtEvenIndexes(null!) == 0);
-        Console.WriteLine(CountEvenNumbersAtEvenIndexes([]) == 0);
+        // Console.WriteLine(CountEvenNumbersAtEvenIndexes([2, 3, 4, 5, 6]) == 3);
+        // Console.WriteLine(CountEvenNumbersAtEvenIndexes([1, 2, 3, 4]) == 0);
+        // Console.WriteLine(CountEvenNumbersAtEvenIndexes([0, 1, 8, 3]) == 2);
+        // Console.WriteLine(CountEvenNumbersAtEvenIndexes(null!) == 0);
+        // Console.WriteLine(CountEvenNumbersAtEvenIndexes([]) == 0);
 
         // ChallengeSolutionsRunner.Run_Four_Dictionary_Work()
         // RunnerChecks.Run();
