4e16d6cde692b2e2dcc1e894cca6db563c67a22b
Grit-Dev <paulmcginley16@outlook.com>
2026-09-09 09:37:47 +0100

    feat: calculate running totals

--- CodingChallenges/Program.cs
+++ CodingChallenges/Program.cs
@@ -45,15 +45,48 @@ public class Program
 
         return strBuilder.ToString();
     }
+
+    public static int[] CalculateRunningTotals(int[] numbers)
+    {
+        if (numbers is null || numbers.Length == 0)
+        {
+            return [];
+        }
+
+        int total = 0;
+        List<int> newList = [];
+
+        foreach(int number in numbers)
+        {
+            newList.Add(total += number);
+        }
+
+        return newList.ToArray();
+    }
     public static void Main(string[] args)
     {
+        // Calculate Running Totals
+        int[] totalsOne = CalculateRunningTotals([2, 4, 5]);
+        Console.WriteLine(totalsOne.Length == 3);
+        Console.WriteLine(totalsOne[0] == 2);
+        Console.WriteLine(totalsOne[1] == 6);
+        Console.WriteLine(totalsOne[2] == 11);
+
+        int[] totalsTwo = CalculateRunningTotals([10, -3, 2]);
+        Console.WriteLine(totalsTwo.Length == 3);
+        Console.WriteLine(totalsTwo[0] == 10);
+        Console.WriteLine(totalsTwo[1] == 7);
+        Console.WriteLine(totalsTwo[2] == 9);
+        Console.WriteLine(CalculateRunningTotals(null!).Length == 0);
+        Console.WriteLine(CalculateRunningTotals([]).Length == 0);
+
         // Clean Extra Spaces Between Words
-        Console.WriteLine(CleanExtraSpacesBetweenWords("  Paul   is  coding  ") == "Paul is coding");
-        Console.WriteLine(CleanExtraSpacesBetweenWords("hello     world") == "hello world");
-        Console.WriteLine(CleanExtraSpacesBetweenWords("one") == "one");
-        Console.WriteLine(CleanExtraSpacesBetweenWords("") == "");
-        Console.WriteLine(CleanExtraSpacesBetweenWords(" ") == "");
-        Console.WriteLine(CleanExtraSpacesBetweenWords(null!) == "");
+        // Console.WriteLine(CleanExtraSpacesBetweenWords("  Paul   is  coding  ") == "Paul is coding");
+        // Console.WriteLine(CleanExtraSpacesBetweenWords("hello     world") == "hello world");
+        // Console.WriteLine(CleanExtraSpacesBetweenWords("one") == "one");
+        // Console.WriteLine(CleanExtraSpacesBetweenWords("") == "");
+        // Console.WriteLine(CleanExtraSpacesBetweenWords(" ") == "");
+        // Console.WriteLine(CleanExtraSpacesBetweenWords(null!) == "");
 
         // Count Even Numbers At Even Indexes
         // Console.WriteLine(CountEvenNumbersAtEvenIndexes([2, 3, 4, 5, 6]) == 3);
