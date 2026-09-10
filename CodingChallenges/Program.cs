2cfa6e97ece47ce0f148dc11ae95ec3f5930d864
Grit-Dev <paulmcginley16@outlook.com>
2026-09-10 09:05:42 +0100

    feat: Convert to simple title case + cw

--- CodingChallenges/Program.cs
+++ CodingChallenges/Program.cs
@@ -32,16 +32,37 @@ public class Program
 
         return counter;
     }
+
+    public static string ConvertToSimpleTitleCase(string input)
+    {
+        if (string.IsNullOrWhiteSpace(input))
+        {
+            return "";
+        }
+
+        return string.Join(" ", input.Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                 .Select(name => char.ToUpper(name[0]) + name.Substring(1).ToLower()));
+    }
+
     public static void Main(string[] args)
     {
+        // Convert To Simple Title Case
+        Console.WriteLine(ConvertToSimpleTitleCase("john SMITH codes") == "John Smith Codes");
+        Console.WriteLine(ConvertToSimpleTitleCase(" sarah CONNOR ") == "Sarah Connor");
+        Console.WriteLine(ConvertToSimpleTitleCase("a BIG day") == "A Big Day");
+        Console.WriteLine(ConvertToSimpleTitleCase("x") == "X");
+        Console.WriteLine(ConvertToSimpleTitleCase("") == "");
+        Console.WriteLine(ConvertToSimpleTitleCase(" ") == "");
+        Console.WriteLine(ConvertToSimpleTitleCase(null!) == "");
+
         // Count Sign Changes
-        Console.WriteLine(CountSignChanges([-1, 2, -3, 0, 4, -5]));
-        Console.WriteLine(CountSignChanges([1, 2, 3]) == 0);
-        Console.WriteLine(CountSignChanges([-1, -2, 3]) == 1);
-        Console.WriteLine(CountSignChanges([0, -1, 1, 0, -2]) == 1);
-        Console.WriteLine(CountSignChanges([7]) == 0);
-        Console.WriteLine(CountSignChanges(null!) == 0);
-        Console.WriteLine(CountSignChanges([]) == 0);
+        // Console.WriteLine(CountSignChanges([-1, 2, -3, 0, 4, -5]));
+        // Console.WriteLine(CountSignChanges([1, 2, 3]) == 0);
+        // Console.WriteLine(CountSignChanges([-1, -2, 3]) == 1);
+        // Console.WriteLine(CountSignChanges([0, -1, 1, 0, -2]) == 1);
+        // Console.WriteLine(CountSignChanges([7]) == 0);
+        // Console.WriteLine(CountSignChanges(null!) == 0);
+        // Console.WriteLine(CountSignChanges([]) == 0);
 
         // ChallengeSolutionsRunner.Run_Four_Dictionary_Work()
         // RunnerChecks.Run();
