8c5f92668a035cbd5f522053226d6bd2b2137e6c
Grit-Dev <paulmcginley16@outlook.com>
2026-09-14 10:55:36 +0100

    feat: Get Two Number Totals Abnove Limit

--- CodingChallenges/Program.cs
+++ CodingChallenges/Program.cs
@@ -24,7 +24,7 @@ public class Program
 
     public static string CreateSlugFromTitle(string input)
     {
-        if(string.IsNullOrWhiteSpace(input))
+        if (string.IsNullOrWhiteSpace(input))
         {
             return "";
         }
@@ -33,11 +33,11 @@ public class Program
 
         input = input.ToLower();
 
-        string [] stringSplit = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        string[] stringSplit = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
 
-        foreach(string str in stringSplit)
+        foreach (string str in stringSplit)
         {
-            if(strBuilder.Length > 0)
+            if (strBuilder.Length > 0)
             {
                 strBuilder.Append('-');
             }
@@ -47,16 +47,55 @@ public class Program
 
         return strBuilder.ToString();
     }
+
+    public static int[] GetTwoNumberTotalsAboveLimit(int[] numbers, int limit)
+    {
+        if (numbers is null || numbers.Length < 2)
+        {
+            return [];
+        }
+
+        List<int> totalPairCounter = [];
+
+        for (int index = 0; index < numbers.Length - 1; index++)
+        {
+            int total = numbers[index] + numbers[index + 1];
+
+            if (total > limit)
+            {
+                totalPairCounter.Add(total);
+            }
+        }
+
+        return totalPairCounter.ToArray();
+    }
     public static void Main(string[] args)
     {
+        // Get Tow Number Total Above Limit
+        int[] totalsOne = GetTwoNumberTotalsAboveLimit([2, 5, 10, 1], 10);
+        Console.WriteLine(totalsOne.Length == 2);
+        Console.WriteLine(totalsOne[0] == 15);
+        Console.WriteLine(totalsOne[1] == 11);
+
+        int[] totalsTwo = GetTwoNumberTotalsAboveLimit([1, 2, 3], 10);
+        Console.WriteLine(totalsTwo.Length == 0);
+
+        int[] totalsThree = GetTwoNumberTotalsAboveLimit([-5, 20, -2, 15], 10);
+        Console.WriteLine(totalsThree.Length == 3);
+        Console.WriteLine(totalsThree[0] == 15);
+        Console.WriteLine(totalsThree[1] == 18);
+        Console.WriteLine(totalsThree[2] == 13);
+        Console.WriteLine(GetTwoNumberTotalsAboveLimit(null!, 10).Length == 0);
+        Console.WriteLine(GetTwoNumberTotalsAboveLimit([], 10).Length == 0);
+
         // Create Slug From Title
-        Console.WriteLine(CreateSlugFromTitle("Hello World") == "hello-world");
-        Console.WriteLine(CreateSlugFromTitle(" Paul Codes Daily ") == "paul-codes-daily");
-        Console.WriteLine(CreateSlugFromTitle("CSharp Practice") == "csharp-practice");
-        Console.WriteLine(CreateSlugFromTitle("One") == "one");
-        Console.WriteLine(CreateSlugFromTitle("") == "");
-        Console.WriteLine(CreateSlugFromTitle(" ") == "");
-        Console.WriteLine(CreateSlugFromTitle(null!) == "");
+        // Console.WriteLine(CreateSlugFromTitle("Hello World") == "hello-world");
+        // Console.WriteLine(CreateSlugFromTitle(" Paul Codes Daily ") == "paul-codes-daily");
+        // Console.WriteLine(CreateSlugFromTitle("CSharp Practice") == "csharp-practice");
+        // Console.WriteLine(CreateSlugFromTitle("One") == "one");
+        // Console.WriteLine(CreateSlugFromTitle("") == "");
+        // Console.WriteLine(CreateSlugFromTitle(" ") == "");
+        // Console.WriteLine(CreateSlugFromTitle(null!) == "");
 
         // Warm up: Count Adjacent Increases. 
         // Console.WriteLine(CountAdjacentIncreases([1, 3, 2, 5]) == 2);
