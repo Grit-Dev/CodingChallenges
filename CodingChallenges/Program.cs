5975e059badd2205526e9d62bf1a2a8a14638daa
Grit-Dev <paulmcginley16@outlook.com>
2026-09-14 10:22:42 +0100

    feat: create slug from title + cw

--- CodingChallenges/Program.cs
+++ CodingChallenges/Program.cs
@@ -1,4 +1,6 @@
-﻿public class Program
+﻿using System.Text;
+
+public class Program
 {
     public static int CountAdjacentIncreases(int[] numbers)
     {
@@ -9,7 +11,7 @@
 
         int counter = 0;
 
-        for (int index = 1; index <= numbers.Length -1; index++)
+        for (int index = 1; index <= numbers.Length - 1; index++)
         {
             if (numbers[index] > numbers[index - 1])
             {
@@ -19,15 +21,50 @@
 
         return counter;
     }
+
+    public static string CreateSlugFromTitle(string input)
+    {
+        if(string.IsNullOrWhiteSpace(input))
+        {
+            return "";
+        }
+
+        var strBuilder = new StringBuilder();
+
+        input = input.ToLower();
+
+        string [] stringSplit = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+        foreach(string str in stringSplit)
+        {
+            if(strBuilder.Length > 0)
+            {
+                strBuilder.Append('-');
+            }
+
+            strBuilder.Append(str);
+        }
+
+        return strBuilder.ToString();
+    }
     public static void Main(string[] args)
     {
+        // Create Slug From Title
+        Console.WriteLine(CreateSlugFromTitle("Hello World") == "hello-world");
+        Console.WriteLine(CreateSlugFromTitle(" Paul Codes Daily ") == "paul-codes-daily");
+        Console.WriteLine(CreateSlugFromTitle("CSharp Practice") == "csharp-practice");
+        Console.WriteLine(CreateSlugFromTitle("One") == "one");
+        Console.WriteLine(CreateSlugFromTitle("") == "");
+        Console.WriteLine(CreateSlugFromTitle(" ") == "");
+        Console.WriteLine(CreateSlugFromTitle(null!) == "");
+
         // Warm up: Count Adjacent Increases. 
-        Console.WriteLine(CountAdjacentIncreases([1, 3, 2, 5]) == 2);
-        Console.WriteLine(CountAdjacentIncreases([5, 4, 3]) == 0);
-        Console.WriteLine(CountAdjacentIncreases([1, 2, 3, 4]) == 3);
-        Console.WriteLine(CountAdjacentIncreases([7]) == 0);
-        Console.WriteLine(CountAdjacentIncreases(null!) == 0);
-        Console.WriteLine(CountAdjacentIncreases([]) == 0);
+        // Console.WriteLine(CountAdjacentIncreases([1, 3, 2, 5]) == 2);
+        // Console.WriteLine(CountAdjacentIncreases([5, 4, 3]) == 0);
+        // Console.WriteLine(CountAdjacentIncreases([1, 2, 3, 4]) == 3);
+        // Console.WriteLine(CountAdjacentIncreases([7]) == 0);
+        // Console.WriteLine(CountAdjacentIncreases(null!) == 0);
+        // Console.WriteLine(CountAdjacentIncreases([]) == 0);
 
         // ChallengeSolutionsRunner.Run_Four_Dictionary_Work()
         // RunnerChecks.Run();
