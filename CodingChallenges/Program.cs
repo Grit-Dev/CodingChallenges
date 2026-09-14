ea3e83814268095890921ff0ba79de41e9236ec8
Grit-Dev <paulmcginley16@outlook.com>
2026-09-14 13:48:28 +0100

    chore: Remove using statement not being used

--- CodingChallenges/Program.cs
+++ CodingChallenges/Program.cs
@@ -1,10 +1,7 @@
-﻿using System.Text;
-
-public class Program
+﻿public class Program
 {
     public static void Main(string[] args)
     {
-
         // ChallengeSolutionsRunner.Run_Four_Dictionary_Work()
         // RunnerChecks.Run();
         // VaultItemInheritanceRunner.Run();
