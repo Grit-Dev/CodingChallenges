6a63d36b4899cf808670310592b95cf74fedbfb4
Grit-Dev <paulmcginley16@outlook.com>
2026-09-15 09:08:35 +0100

    feat Remove Short Words and Lowercase + cw

--- CodingChallenges/Program.cs
+++ CodingChallenges/Program.cs
@@ -19,15 +19,36 @@
 
         return counter;
     }
+
+    public static string RemoveShortWordsAndLowercase(string input)
+    {
+        if (string.IsNullOrWhiteSpace(input))
+        {
+            return "";
+        }
+
+        return string.Join(' ', input.Split(' ', StringSplitOptions.RemoveEmptyEntries)
+        .Where(ss => ss.Length >= 4)
+        .Select(ss => ss.ToLower()));
+    }
     public static void Main(string[] args)
     {
+        // Remove Short Words And Lowercase
+        Console.WriteLine(RemoveShortWordsAndLowercase("Hi Paul is Coding Today")  == "paul coding today");
+        Console.WriteLine(RemoveShortWordsAndLowercase("a big red dragon") == "dragon");
+        Console.WriteLine(RemoveShortWordsAndLowercase("to be or no") == "");
+        Console.WriteLine(RemoveShortWordsAndLowercase(" HELLO world ") == "hello world");
+        Console.WriteLine(RemoveShortWordsAndLowercase("") == "");
+        Console.WriteLine(RemoveShortWordsAndLowercase(" ") == "");
+        Console.WriteLine(RemoveShortWordsAndLowercase(null!) == "");
+
         // Count Numbers Divisible By Two But Not Three
-        Console.WriteLine(CountNumbersDivisibleByTwoButNotThree([2, 3, 4, 6, 8, 9, 12]) == 3);
-        Console.WriteLine(CountNumbersDivisibleByTwoButNotThree([6, 12, 18]) == 0);
-        Console.WriteLine(CountNumbersDivisibleByTwoButNotThree([0, 2, 4]) == 2);
-        Console.WriteLine(CountNumbersDivisibleByTwoButNotThree([1, 3, 5]) == 0);
-        Console.WriteLine(CountNumbersDivisibleByTwoButNotThree(null!) == 0);
-        Console.WriteLine(CountNumbersDivisibleByTwoButNotThree([]) == 0);
+        // Console.WriteLine(CountNumbersDivisibleByTwoButNotThree([2, 3, 4, 6, 8, 9, 12]) == 3);
+        // Console.WriteLine(CountNumbersDivisibleByTwoButNotThree([6, 12, 18]) == 0);
+        // Console.WriteLine(CountNumbersDivisibleByTwoButNotThree([0, 2, 4]) == 2);
+        // Console.WriteLine(CountNumbersDivisibleByTwoButNotThree([1, 3, 5]) == 0);
+        // Console.WriteLine(CountNumbersDivisibleByTwoButNotThree(null!) == 0);
+        // Console.WriteLine(CountNumbersDivisibleByTwoButNotThree([]) == 0);
 
         // ChallengeSolutionsRunner.Run_Four_Dictionary_Work()
         // RunnerChecks.Run();
