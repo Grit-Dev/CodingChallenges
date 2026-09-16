edcdb13e2ac15b9872b856f28f8004a0aafbcea9
Grit-Dev <paulmcginley16@outlook.com>
2026-09-16 08:55:39 +0100

    feat: Extract mention Usernames + cw

--- CodingChallenges/Program.cs
+++ CodingChallenges/Program.cs
@@ -1,21 +1,23 @@
-﻿public class Program
+﻿using System.Xml;
+
+public class Program
 {
     public static int CountNumbersDivisibleByTwoOrThreeButNotBoth(int[] numbers)
     {
-        if(numbers is null || numbers.Length == 0)
+        if (numbers is null || numbers.Length == 0)
         {
             return 0;
         }
 
         int counter = 0;
 
-        foreach(int number in numbers)
+        foreach (int number in numbers)
         {
-            if(number % 2 == 0 && number % 3 == 0)
+            if (number % 2 == 0 && number % 3 == 0)
             {
                 continue;
             }
-            else if(number % 2 == 0 || number % 3 == 0 && number != 0)
+            else if (number % 2 == 0 || number % 3 == 0)
             {
                 counter++;
             }
@@ -23,16 +25,66 @@
 
         return counter;
     }
+
+    public static string[] ExtractMentionUsernames(string input)
+    {
+        if (string.IsNullOrWhiteSpace(input))
+        {
+            return [];
+        }
+
+        List<string> newList = [];
+        string[] stringSplit = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+        foreach (string str in stringSplit)
+        {
+            int indexOfAtSymbol = str.IndexOf('@');
+            int indexOfAtSymbolCount = str.Count(i => i == '@');
+
+            if (indexOfAtSymbolCount > 1)
+            {
+                continue;
+            }
+
+            if (indexOfAtSymbol > 0 || indexOfAtSymbol < 0)
+            {
+                continue;
+            }
+
+            if (str.Length > 1)
+            {
+                newList.Add(str.Substring(1));
+            }
+        }
+
+        return newList.ToArray();
+    }
+
     public static void Main(string[] args)
     {
+        // Extract Mention Usernames 
+        string[] mentionsOne = ExtractMentionUsernames("hello @Paul and @Sarah");
+        Console.WriteLine(mentionsOne.Length == 2);
+        Console.WriteLine(mentionsOne[0] == "Paul");
+        Console.WriteLine(mentionsOne[1] == "Sarah");
+
+        string[] mentionsTwo = ExtractMentionUsernames("@one @ @Two");
+        Console.WriteLine(mentionsTwo.Length == 2);
+        Console.WriteLine(mentionsTwo[0] == "one");
+        Console.WriteLine(mentionsTwo[1] == "Two");
+        Console.WriteLine(ExtractMentionUsernames("no mentions here").Length == 0);
+        Console.WriteLine(ExtractMentionUsernames("").Length == 0);
+        Console.WriteLine(ExtractMentionUsernames(" ").Length == 0);
+        Console.WriteLine(ExtractMentionUsernames(null!).Length == 0);
+
         // Warm up: Count Numbers divisible by two or three but noth Both. 
-        Console.WriteLine(CountNumbersDivisibleByTwoOrThreeButNotBoth([2, 3, 4, 6, 9, 12, 15]) == 5);
-        Console.WriteLine(CountNumbersDivisibleByTwoOrThreeButNotBoth([6, 12, 18]) == 0);
-        Console.WriteLine(CountNumbersDivisibleByTwoOrThreeButNotBoth([2, 4, 8]) == 3);
-        Console.WriteLine(CountNumbersDivisibleByTwoOrThreeButNotBoth([3, 9, 15]) == 3);
-        Console.WriteLine(CountNumbersDivisibleByTwoOrThreeButNotBoth([0, 2, 6]) == 1);
-        Console.WriteLine(CountNumbersDivisibleByTwoOrThreeButNotBoth(null!) == 0);
-        Console.WriteLine(CountNumbersDivisibleByTwoOrThreeButNotBoth([]) == 0);
+        // Console.WriteLine(CountNumbersDivisibleByTwoOrThreeButNotBoth([2, 3, 4, 6, 9, 12, 15]) == 5);
+        // Console.WriteLine(CountNumbersDivisibleByTwoOrThreeButNotBoth([6, 12, 18]) == 0);
+        // Console.WriteLine(CountNumbersDivisibleByTwoOrThreeButNotBoth([2, 4, 8]) == 3);
+        // Console.WriteLine(CountNumbersDivisibleByTwoOrThreeButNotBoth([3, 9, 15]) == 3);
+        // Console.WriteLine(CountNumbersDivisibleByTwoOrThreeButNotBoth([0, 2, 6]) == 1);
+        // Console.WriteLine(CountNumbersDivisibleByTwoOrThreeButNotBoth(null!) == 0);
+        // Console.WriteLine(CountNumbersDivisibleByTwoOrThreeButNotBoth([]) == 0);
 
         // ChallengeSolutionsRunner.Run_Four_Dictionary_Work()
         // RunnerChecks.Run();
