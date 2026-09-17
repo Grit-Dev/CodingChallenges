ceb970a9745b68a9e8a1b50688337227498bc09f
Grit-Dev <paulmcginley16@outlook.com>
2026-09-17 09:39:43 +0100

    feat: Find Highest Running Total + cw

--- CodingChallenges/Program.cs
+++ CodingChallenges/Program.cs
@@ -22,12 +22,12 @@
 
     public static string MaskAllButLastThreeCharacters(string input)
     {
-        if(string.IsNullOrWhiteSpace(input))
+        if (string.IsNullOrWhiteSpace(input))
         {
             return "";
         }
 
-        if(input.Length  <= 3)
+        if (input.Length <= 3)
         {
             return input;
         }
@@ -39,16 +39,47 @@
 
         return startOfString + endOfString;
     }
+
+    public static int? FindHighestRunningTotal(int[] numbers)
+    {
+        if(numbers is null || numbers.Length == 0)
+        {
+            return null;
+        }
+
+        int total = 0;
+        int? highestRunningTotal = null;
+
+        foreach(int number in numbers)
+        {
+            total += number;
+
+            if(highestRunningTotal is null || total > highestRunningTotal)
+            {
+                highestRunningTotal = total;
+            }
+        }
+
+        return highestRunningTotal;
+    }
     public static void Main(string[] args)
     {
+        // Find Highest Running Total
+        Console.WriteLine(FindHighestRunningTotal([3, -1, 5, -10]) == 7);
+        Console.WriteLine(FindHighestRunningTotal([5, -2, -10, 20]) == 13);
+        Console.WriteLine(FindHighestRunningTotal([-2, -3, -1]) == -2);
+        Console.WriteLine(FindHighestRunningTotal([1, 1, -5]) == 2);
+        Console.WriteLine(FindHighestRunningTotal(null!) == null);
+        Console.WriteLine(FindHighestRunningTotal([]) == null);
+
         // Mask All But Last Three Characters 
-        Console.WriteLine(MaskAllButLastThreeCharacters("abcdef") == "***def");
-        Console.WriteLine(MaskAllButLastThreeCharacters("Paul") == "*aul");
-        Console.WriteLine(MaskAllButLastThreeCharacters("abc") == "abc");
-        Console.WriteLine(MaskAllButLastThreeCharacters("a") == "a");
-        Console.WriteLine(MaskAllButLastThreeCharacters("") == "");
-        Console.WriteLine(MaskAllButLastThreeCharacters("   ") == "");
-        Console.WriteLine(MaskAllButLastThreeCharacters(null!) == "");
+        // Console.WriteLine(MaskAllButLastThreeCharacters("abcdef") == "***def");
+        // Console.WriteLine(MaskAllButLastThreeCharacters("Paul") == "*aul");
+        // Console.WriteLine(MaskAllButLastThreeCharacters("abc") == "abc");
+        // Console.WriteLine(MaskAllButLastThreeCharacters("a") == "a");
+        // Console.WriteLine(MaskAllButLastThreeCharacters("") == "");
+        // Console.WriteLine(MaskAllButLastThreeCharacters("   ") == "");
+        // Console.WriteLine(MaskAllButLastThreeCharacters(null!) == "");
 
         // Count Numbers Greater Than Previous number
         // Console.WriteLine(CountNumbersGreaterThanPreviousNumber([1, 3, 2, 5]) == 2);
