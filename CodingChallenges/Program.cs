0987ecbe943c56c8c63472f477d9d87320c85f64
Paul McGinley <paulmcginley16@outlook.com>
2026-09-24 18:10:59 +0100

    Find Words With No Repeated Letter + cw

--- CodingChallenges/Program.cs
+++ CodingChallenges/Program.cs
@@ -8,16 +8,14 @@
         }
 
         if (numbers.Length == 1)
-        { 
+        {
             return [numbers[0]];
         }
 
         int[] result = new int[numbers.Length];
 
-        // Move last element to the front
         result[0] = numbers[numbers.Length - 1];
 
-        // Shift everything else right by one
         for (int index = 0; index < numbers.Length - 1; index++)
         {
             result[index + 1] = numbers[index];
@@ -26,8 +24,55 @@
         return result;
     }
 
+    public static string[] FindWordsWithNoRepeatedLetters(string input)
+    {
+        if (string.IsNullOrWhiteSpace(input))
+        {
+            return [];
+        }
+
+        List<string> newList = [];
+        string[] splitString = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+       foreach (string word in splitString)
+       {
+            List<char> UniqueChr = [];
+
+            foreach (char chr in word)
+            {
+                if (!UniqueChr.Contains(chr))
+                {
+                    UniqueChr.Add(chr);
+                }
+            }
+
+            if (UniqueChr.Count == word.Length)
+            {
+                newList.Add(word);
+            }
+       }
+
+       return newList.ToArray();
+    }
+
     public static void Main(string[] args)
     {
+        // Unique Letters:
+        string[] uniqueLettersOne = FindWordsWithNoRepeatedLetters("cat apple sword moon");
+
+        Console.WriteLine(uniqueLettersOne.Length == 2);
+        Console.WriteLine(uniqueLettersOne[0] == "cat");
+        Console.WriteLine(uniqueLettersOne[1] == "sword");
+
+        string[] uniqueLettersTwo = FindWordsWithNoRepeatedLetters("Book test abc");
+
+        Console.WriteLine(uniqueLettersTwo.Length == 1);
+        Console.WriteLine(uniqueLettersTwo[0] == "abc");
+
+        Console.WriteLine(FindWordsWithNoRepeatedLetters("hello moon").Length == 0);
+        Console.WriteLine(FindWordsWithNoRepeatedLetters("").Length == 0);
+        Console.WriteLine(FindWordsWithNoRepeatedLetters(null).Length == 0);
+
         // Warm Up: Rotate Right By One
         int[] rotateOne = RotateRightByOne([1, 2, 3, 4]);
 
