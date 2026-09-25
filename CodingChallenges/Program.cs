d7682c4661dc8ce9c2037a5e3cca6c7dc43ccb89
Paul McGinley <paulmcginley16@outlook.com>
2026-09-25 10:38:00 +0100

    refactor: Fixed up CW lines within program files

--- CodingChallenges/Program.cs
+++ CodingChallenges/Program.cs
@@ -107,23 +107,19 @@
 
         // Unique Letters:
         string[] uniqueLettersOne = FindWordsWithNoRepeatedLetters("cat apple sword moon");
-
         Console.WriteLine(uniqueLettersOne.Length == 2);
         Console.WriteLine(uniqueLettersOne[0] == "cat");
         Console.WriteLine(uniqueLettersOne[1] == "sword");
 
         string[] uniqueLettersTwo = FindWordsWithNoRepeatedLetters("Book test abc");
-
         Console.WriteLine(uniqueLettersTwo.Length == 1);
         Console.WriteLine(uniqueLettersTwo[0] == "abc");
-
         Console.WriteLine(FindWordsWithNoRepeatedLetters("hello moon").Length == 0);
         Console.WriteLine(FindWordsWithNoRepeatedLetters("").Length == 0);
         Console.WriteLine(FindWordsWithNoRepeatedLetters(null!).Length == 0);
 
         // Warm Up: Rotate Right By One
         int[] rotateOne = RotateRightByOne([1, 2, 3, 4]);
-
         Console.WriteLine(rotateOne.Length == 4);
         Console.WriteLine(rotateOne[0] == 4);
         Console.WriteLine(rotateOne[1] == 1);
@@ -131,18 +127,14 @@
         Console.WriteLine(rotateOne[3] == 3);
 
         int[] rotateTwo = RotateRightByOne([10, 20]);
-
         Console.WriteLine(rotateTwo[0] == 20);
         Console.WriteLine(rotateTwo[1] == 10);
 
         int[] rotateThree = RotateRightByOne([7]);
-
         Console.WriteLine(rotateThree.Length == 1);
         Console.WriteLine(rotateThree[0] == 7);
-
         Console.WriteLine(RotateRightByOne(null!).Length == 0);
         Console.WriteLine(RotateRightByOne([]).Length == 0);
-
 
         // ChallengeSolutionsRunner.Run_Four_Dictionary_Work()
         // RunnerChecks.Run();
